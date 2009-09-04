59adc608d99eefc6abf409a436bf51fc2c0bea2e
Jonathan Newman <jonathan.newman@catalyst.net.nz>
2009-09-04 18:20:24 +1200

    admin/settings: added positions to admin tree

---
+++ admin/settings/positions.php
@@ -0,0 +1,19 @@
+<?php // $Id$
+
+// This file defines settingpages and externalpages under the "positions" category
+
+    $ADMIN->add('positions', new admin_externalpage('positionbrowselist', get_string('positionbrowselist', 'admin'), "$CFG->wwwroot/$CFG->admin/position.php",
+            array('moodle/category:manage', 'moodle/course:create')));
+
+    $ADMIN->add('positions', new admin_externalpage('positionbulkaction', get_string('positionbulkaction', 'admin'), $CFG->wwwroot . '/course/index.php?categoryedit=on',
+            array('moodle/category:manage', 'moodle/course:create')));
+
+    $ADMIN->add('positions', new admin_externalpage('positionaddnew', get_string('positionaddnew', 'admin'), $CFG->wwwroot . '/course/index.php?categoryedit=on',
+            array('moodle/category:manage', 'moodle/course:create')));
+
+    $ADMIN->add('positions', new admin_externalpage('positionupload', get_string('positionupload', 'admin'), $CFG->wwwroot . '/course/index.php?categoryedit=on',
+            array('moodle/category:manage', 'moodle/course:create')));
+
+    $ADMIN->add('positions', new admin_externalpage('positioncustomfields', get_string('positioncustomfields', 'admin'), $CFG->wwwroot . '/course/index.php?categoryedit=on',
+            array('moodle/category:manage', 'moodle/course:create')));
+?>
