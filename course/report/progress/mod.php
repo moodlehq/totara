037716e17835a63fa2c671990d888f0ebc6b720f
Aaron Barnes <aaronb@catalyst.net.nz>
2009-09-10 14:34:26 +1200

    course/report/progress: Adding backported progress report

---
+++ course/report/progress/mod.php
@@ -0,0 +1,15 @@
+<?php  //$Id$
+
+    if (!defined('MOODLE_INTERNAL')) {
+        die('Direct access to this script is forbidden.'); // It must be included from a Moodle page
+    }
+
+    if (has_capability('coursereport/progress:view', $context)) {
+        $completion = new completion_info($course);
+        if ($completion->is_enabled()) {
+            echo '<p>';
+            echo '<a href="'.$CFG->wwwroot.'/course/report/progress/index.php?course='.$course->id.'">'.get_string('completionreport','completion').'</a>';
+            echo '</p>';
+        }
+    }
+?>
