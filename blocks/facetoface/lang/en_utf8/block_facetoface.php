c32a5a0710da0c0f2d738282cd941dea9ca23af5
Jonathan Newman <jonathan.newman@catalyst.net.nz>
2009-09-14 11:36:56 +1200

    blocks/facetoface: adding block

---
+++ blocks/facetoface/lang/en_utf8/block_facetoface.php
@@ -0,0 +1,22 @@
+<?php
+
+$string['allsessions'] = 'All sessions';
+$string['allfuturesessions'] = 'All future sessions';
+$string['apply'] = 'Apply';
+$string['daterange'] = 'Date range:';
+$string['exportsessiondates'] = 'Export session dates';
+$string['fixdates'] = 'Fix session dates';
+$string['formaltitle'] = 'Face-to-face';
+$string['listsessiondates'] = 'List Face-to-face Session Dates';
+$string['nbbookings'] = 'Bookings';
+$string['sessiondatesviewattendees'] = 'You can view attendees in the following sessions:';
+$string['sessiondatesviewattendeeszero'] = 'You cannot view the attendees of any session in the given date range.';
+$string['sessiondatestable'] = 'List of session details along with number of bookings';
+$string['sessionlist'] = 'Session list';
+$string['signedupin'] = 'You are signed-up in the following sessions:';
+$string['signedupinzero'] = 'No sign-ups could be found in the given date range.';
+$string['upcomingsessions'] = 'Upcoming sessions';
+$string['viewexportsessions'] = 'View/export sessions';
+$string['xrecords'] = '$a records';
+
+?>
