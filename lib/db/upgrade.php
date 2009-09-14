80d5bce1fa2682b305eda8684b27b8e076f1c8ab
Aaron Barnes <aaronb@catalyst.net.nz>
2009-09-15 11:37:58 +1200

    lib/db: Adding database structure for conditional activities

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -3240,6 +3240,57 @@ function xmldb_main_upgrade($oldversion=0) {
         /// Main savepoint reached
         upgrade_main_savepoint($result, 2007101552);
     }
+
+    if ($result && $oldversion < 2007101553) {
+
+    /// Define field availablefrom to be added to course_modules
+        $table = new XMLDBTable('course_modules');
+        $field = new XMLDBField('availablefrom');
+        $field->setAttributes(XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'completionexpected');
+
+    /// Conditionally launch add field availablefrom
+        $result = $result && add_field($table, $field);
+
+    /// Define field availableuntil to be added to course_modules
+        $field = new XMLDBField('availableuntil');
+        $field->setAttributes(XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'availablefrom');
+
+    /// Conditionally launch add field availableuntil
+        $result = $result && add_field($table, $field);
+
+    /// Define field showavailability to be added to course_modules
+        $field = new XMLDBField('showavailability');
+        $field->setAttributes(XMLDB_TYPE_INTEGER, '1', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, '0', 'availableuntil');
+
+    /// Conditionally launch add field showavailability
+        $result = $result && add_field($table, $field);
+
+    /// Define table course_modules_availability to be created
+        $table = new XMLDBTable('course_modules_availability');
+
+    /// Adding fields to table course_modules_availability
+        $table->addFieldInfo('id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, XMLDB_SEQUENCE, null, null, null);
+        $table->addFieldInfo('coursemoduleid', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null, null, null);
+        $table->addFieldInfo('sourcecmid', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null, null, null, null, null);
+        $table->addFieldInfo('requiredcompletion', XMLDB_TYPE_INTEGER, '1', XMLDB_UNSIGNED, null, null, null, null, null);
+        $table->addFieldInfo('gradeitemid', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null, null, null, null, null);
+        $table->addFieldInfo('grademin', XMLDB_TYPE_NUMBER, '10, 5', null, null, null, null, null, null);
+        $table->addFieldInfo('grademax', XMLDB_TYPE_NUMBER, '10, 5', null, null, null, null, null, null);
+
+    /// Adding keys to table course_modules_availability
+        $table->addKeyInfo('primary', XMLDB_KEY_PRIMARY, array('id'));
+        $table->addKeyInfo('coursemoduleid', XMLDB_KEY_FOREIGN, array('coursemoduleid'), 'course_modules', array('id'));
+        $table->addKeyInfo('sourcecmid', XMLDB_KEY_FOREIGN, array('sourcecmid'), 'course_modules', array('id'));
+        $table->addKeyInfo('gradeitemid', XMLDB_KEY_FOREIGN, array('gradeitemid'), 'grade_items', array('id'));
+
+    /// Conditionally launch create table for course_modules_availability
+        if (!table_exists($table)) {
+            create_table($table);
+        }
+
+    /// Main savepoint reached
+        upgrade_main_savepoint($result, 2007101553);
+    }
     
     return $result;
 }
