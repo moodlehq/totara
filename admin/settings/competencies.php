d55b623cfde69a462378a3a9ef7cc1d1343c34cf
Jonathan Newman <jonathan.newman@catalyst.net.nz>
2009-10-01 18:22:42 +1300

    competencies: move Site Admin menu strings to competencies lang pack

--- admin/settings/competencies.php
+++ admin/settings/competencies.php
@@ -2,20 +2,20 @@
 
 // This file defines settingpages and externalpages under the "competencies" category
 
-    $ADMIN->add('competencies', new admin_externalpage('competencyframeworkmanage', get_string('competencyframeworkmanage', 'admin'), "$CFG->wwwroot/competencies/frameworks/index.php",
+    $ADMIN->add('competencies', new admin_externalpage('competencyframeworkmanage', get_string('competencyframeworkmanage', 'competencies'), "$CFG->wwwroot/competencies/frameworks/index.php",
             array('moodle/local:viewcompetencies')));
 
-    $ADMIN->add('competencies', new admin_externalpage('competencymanage', get_string('competencymanage', 'admin'), "$CFG->wwwroot/competencies/index.php",
+    $ADMIN->add('competencies', new admin_externalpage('competencymanage', get_string('competencymanage', 'competencies'), "$CFG->wwwroot/competencies/index.php",
             array('moodle/local:viewcompetencies')));
 
-    $ADMIN->add('competencies', new admin_externalpage('competencybulkaction', get_string('competencybulkaction', 'admin'), "$CFG->wwwroot/competencies/bulk.php",
+    $ADMIN->add('competencies', new admin_externalpage('competencybulkaction', get_string('competencybulkaction', 'competencies'), "$CFG->wwwroot/competencies/bulk.php",
             array('moodle/local:updatecompetencies', 'moodle/local:deletecompetenties')));
 
-    $ADMIN->add('competencies', new admin_externalpage('competencyupload', get_string('competencyupload', 'admin'), "$CFG->wwwroot/competencies/upload.php",
+    $ADMIN->add('competencies', new admin_externalpage('competencyupload', get_string('competencyupload', 'competencies'), "$CFG->wwwroot/competencies/upload.php",
             array('moodle/local:updatecompetencies')));
 
-    $ADMIN->add('competencies', new admin_externalpage('competencydepthcustomfields', get_string('competencycustomfields', 'admin'), "$CFG->wwwroot/competencies/depth/customfields/index.php",
+    $ADMIN->add('competencies', new admin_externalpage('competencydepthcustomfields', get_string('competencycustomfields', 'competencies'), "$CFG->wwwroot/competencies/depth/customfields/index.php",
             array('moodle/local:updatecompetencies')));
 
-    $ADMIN->add('competencies', new admin_externalpage('competencyscales', get_string('competencyscales', 'admin'), "$CFG->wwwroot/competencies/scale/index.php",
+    $ADMIN->add('competencies', new admin_externalpage('competencyscales', get_string('competencyscales', 'competencies'), "$CFG->wwwroot/competencies/scale/index.php",
             array('moodle/local:viewcompetencies')));
