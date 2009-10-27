25f3c177f878c9f71c9a09fc11572da8105f75a8
Aaron Barnes <aaronb@catalyst.net.nz>
2009-10-27 16:39:21 +1300

    competency/frameworks/delete.php: Hierarchylibify it some more

--- competency/frameworks/delete.php
+++ competency/frameworks/delete.php
@@ -23,7 +23,7 @@ $hierarchy->prefix = 'competency';
 // Setup page and check permissions
 admin_externalpage_setup($hierarchy->prefix.'frameworkmanage');
 
-require_capability('moodle/local:deletecompetencyframeworks', $sitecontext);
+require_capability('moodle/local:delete'.$hierarchy->prefix.'frameworks', $sitecontext);
 
 $framework = $hierarchy->get_framework($id);
 
@@ -34,11 +34,11 @@ $framework = $hierarchy->get_framework($id);
 admin_externalpage_print_header();
 
 if (!$delete) {
-    $strdelete = get_string('deletecheckframework', 'competency');
+    $strdelete = get_string('deletecheckframework', $hierarchy->prefix);
 
     notice_yesno("$strdelete<br /><br />" . format_string($framework->fullname),
-                 "{$CFG->wwwroot}/competency/frameworks/delete.php?id={$framework->id}&amp;delete=".md5($framework->timemodified)."&amp;sesskey={$USER->sesskey}",
-                 "{$CFG->wwwroot}/competency/frameworks/index.php");
+                 "{$CFG->wwwroot}/{$hierarchy->prefix}/frameworks/delete.php?id={$framework->id}&amp;delete=".md5($framework->timemodified)."&amp;sesskey={$USER->sesskey}",
+                 "{$CFG->wwwroot}/{$hierarchy->prefix}/frameworks/index.php");
 
     print_footer();
     exit;
@@ -57,10 +57,10 @@ if (!confirm_sesskey()) {
     print_error('confirmsesskeybad', 'error');
 }
 
-add_to_log(SITEID, 'competencyframeworks', 'delete', "view.php?frameworkid=$framework->id", "$framework->fullname (ID $framework->id)");
+add_to_log(SITEID, $hierarchy->prefix.'frameworks', 'delete', "view.php?frameworkid=$framework->id", "$framework->fullname (ID $framework->id)");
 
 $hierarchy->delete_framework();
 
-print_heading(get_string('deletedframework', 'competency', format_string($framework->fullname)));
-print_continue("{$CFG->wwwroot}/competency/frameworks/index.php");
+print_heading(get_string('deletedframework', $hierarchy->prefix, format_string($framework->fullname)));
+print_continue("{$CFG->wwwroot}/{$hierarchy->prefix}/frameworks/index.php");
 print_footer();
