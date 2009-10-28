576ee34520fed8bf2b6af90c1e10c2efb1c9e997
Simon Coggins <simonc@catalyst.net.nz>
2009-10-28 16:48:28 +1300

    Added form for show options

---
+++ competency/show_options_form.php
@@ -0,0 +1,28 @@
+<?php //$Id$
+
+require_once($CFG->libdir.'/formslib.php');
+
+class competency_show_options_form extends moodleform {
+
+    function definition() {
+        $mform =& $this->_form;
+
+        $mform->addElement('header', 'displayoptions', get_string('displayoptions','competency'));
+
+        $mform->addElement('checkbox', 'hidecustomfields', get_string('hidecustomfields', 'competency'));
+        $mform->setType('hidecustomfields', PARAM_BOOL);
+
+        $mform->addElement('checkbox', 'showitemfullname', get_string('showitemfullname', 'competency'));
+        $mform->setType('showitemfullname', PARAM_BOOL);
+
+        $mform->addElement('checkbox', 'showdepthfullname', get_string('showdepthfullname', 'competency'));
+        $mform->setType('showdepthfullname', PARAM_BOOL);
+
+        $mform->addElement('hidden', 'frameworkid', $this->_customdata['framework']->id);
+        $mform->setType('frameworkid', PARAM_INT);
+
+        $mform->addElement('submit', 'submitbutton', get_string('savechanges'));
+    }
+}
+
+
