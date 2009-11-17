106f151c8e28318b962cd62007d795b64d1ceb8b
Aaron Barnes <aaronb@catalyst.net.nz>
2009-11-17 17:10:38 +1300

    course/user.php: Backport of user completion report

---
+++ lang/en_utf8/coursereport_completion.php
@@ -0,0 +1,13 @@
+<?php
+
+$string['activitiescomplete'] = 'Activities complete';
+$string['complete'] = 'Courses completed';
+$string['completion:view'] = 'View completion report';
+$string['completiondate'] = 'Completion Date';
+$string['completions'] = 'Course completions';
+$string['detailedview'] = 'Detail view';
+$string['incomplete'] = 'Incomplete';
+$string['inprogress'] = 'Courses in progress';
+$string['nocompletioncoursesenroled'] = 'User not enrolled in any courses with completion enabled';
+$string['nocompletions'] = 'No completion information exists for this user';
+$string['unstarted'] = 'Courses not yet started';
