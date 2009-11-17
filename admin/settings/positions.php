6e99e3115828a497a13009cac040242c1f2b19e7
Jonathan Newman <jonathan.newman@catalyst.net.nz>
2009-11-17 21:26:31 +1300

    admin/settings/positions: remove positionupload and positioncustomfields

--- admin/settings/positions.php
+++ admin/settings/positions.php
@@ -8,9 +8,4 @@
     $ADMIN->add('positions', new admin_externalpage('positionmanage', get_string('positionmanage', 'position'), $CFG->wwwroot . '/hierarchy/index.php?type=position',
             array('moodle/local:updateposition')));
 
-    $ADMIN->add('positions', new admin_externalpage('positionupload', get_string('positionupload', 'position'), "$CFG->wwwroot/$CFG->admin/position/upload.php",
-            array('moodle/local:updateposition')));
-
-    $ADMIN->add('positions', new admin_externalpage('positiondepthcustomfields', get_string('positioncustomfields', 'position'), $CFG->wwwroot . '/position/depth/customfields/index.php',
-            array('moodle/local:viewposition')));
 ?>
