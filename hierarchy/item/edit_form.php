d41d66aecdab77c566382bbce9d9217ab748a43c
Jonathan Newman <jonathan.newman@catalyst.net.nz>
2009-11-17 21:12:46 +1300

    hierarchy/item/edit_form: replace references to competency with type

--- hierarchy/item/edit_form.php
+++ hierarchy/item/edit_form.php
@@ -11,7 +11,7 @@ class item_edit_form extends moodleform {
 
         $mform =& $this->_form;
 
-        // Get all competencies in this framework that we can use
+        // Get all items in this framework that we can use
         // as parents
         $type = $this->_customdata['type'];
         $item = $this->_customdata['item'];
@@ -90,37 +90,37 @@ class item_edit_form extends moodleform {
         /// Print the required moodle fields first
         $mform->addElement('header', 'moodle', $strgeneral);
 
-        $mform->addElement('text', 'framework', get_string('framework', 'competency'));
-        $mform->setHelpButton('framework', array('competencyframework', get_string('framework', 'competency')), true);
+        $mform->addElement('text', 'framework', get_string('framework', $type));
+        $mform->setHelpButton('framework', array($type.'framework', get_string('framework', $type)), true);
         $mform->hardFreeze('framework');
 
-        $mform->addElement('select', 'parentid', get_string('parent', 'competency'), $parents);
-        $mform->setHelpButton('parentid', array('competencyparent', get_string('parent', 'competency')), true);
+        $mform->addElement('select', 'parentid', get_string('parent', $type), $parents);
+        $mform->setHelpButton('parentid', array($type.'parent', get_string('parent', $type)), true);
         // If we only have a "Top" placeholder parentid, lock it
         if (count($parents) <= 1) {
             $mform->setDefault('parentid', 0);
             $mform->hardFreeze('parentid');
         }
 
-        $mform->addElement('text', 'fullname', get_string('fullname', 'competency'), 'maxlength="254" size="50"');
-        $mform->setHelpButton('fullname', array('competencyfullname', get_string('fullname', 'competency')), true);
-        $mform->addRule('fullname', get_string('missingfullname', 'competency'), 'required', null, 'client');
+        $mform->addElement('text', 'fullname', get_string('fullname', $type), 'maxlength="254" size="50"');
+        $mform->setHelpButton('fullname', array($type.'fullname', get_string('fullname', $type)), true);
+        $mform->addRule('fullname', get_string('missingfullname', $type), 'required', null, 'client');
         $mform->setType('fullname', PARAM_MULTILANG);
 
-        $mform->addElement('text', 'shortname', get_string('shortname', 'competency'), 'maxlength="100" size="20"');
-        $mform->setHelpButton('shortname', array('competencyshortname', get_string('shortname', 'competency')), true);
-        $mform->addRule('shortname', get_string('missingshortname', 'competency'), 'required', null, 'client');
+        $mform->addElement('text', 'shortname', get_string('shortname', $type), 'maxlength="100" size="20"');
+        $mform->setHelpButton('shortname', array($type.'shortname', get_string('shortname', $type)), true);
+        $mform->addRule('shortname', get_string('missingshortname', $type), 'required', null, 'client');
         $mform->setType('shortname', PARAM_MULTILANG);
 
-        $mform->addElement('text', 'idnumber', get_string('idnumber', 'competency'), 'maxlength="100"  size="10"');
-        $mform->setHelpButton('idnumber', array('competencyidnumber', get_string('idnumber', 'competency')), true);
+        $mform->addElement('text', 'idnumber', get_string('idnumber', $type), 'maxlength="100"  size="10"');
+        $mform->setHelpButton('idnumber', array($type.'idnumber', get_string('idnumber', $type)), true);
         $mform->setType('idnumber', PARAM_RAW);
 
         $mform->addElement('htmleditor', 'description', get_string('description'), array('rows'=> '10', 'cols'=>'65'));
         $mform->setHelpButton('description', array('text', get_string('helptext')), true);
         $mform->setType('description', PARAM_RAW);
 
-        /// Next show the custom fields if we're editing an existing competency (otherwise we don't know the depthid)
+        /// Next show the custom fields if we're editing an existing items (otherwise we don't know the depthid)
         if ($item->id) {
             $mform->addElement('hidden', 'depthid', $item->depthid);
             customfield_definition($mform, $item->id, $type, $item->depthid, $type.'_depth');
@@ -133,10 +133,11 @@ class item_edit_form extends moodleform {
 
         $mform =& $this->_form;
         $itemid = $mform->getElementValue('id');
+        $type   = $mform->getElementValue('type');
 
-        if ($item = get_record('competency', 'id', $itemid)) {
+        if ($item = get_record($type, 'id', $itemid)) {
 
-            customfield_definition_after_data($mform, $item->id, 'competency', $item->depthid, 'competency_depth');
+            customfield_definition_after_data($mform, $item->id, $type, $item->depthid, $type.'_depth');
 
         }
 
