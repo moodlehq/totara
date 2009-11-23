8a0d080c600dbff7320e3657b427cc40b005c3f0
Simon Coggins <simonc@catalyst.net.nz>
2009-11-23 15:05:45 +1300

    Fixed bug that caused download button always downloaded default framework

--- hierarchy/download_form.php
+++ hierarchy/download_form.php
@@ -7,9 +7,10 @@ class hierarchy_download_form extends moodleform {
     function definition() {
         $mform =& $this->_form;
         $datasent = $this->_customdata;
-
+        $frameworkid = $this->_customdata['frameworkid'];
         $mform->addElement('hidden', 'type', $datasent['type']);
 
+        $mform->addElement('hidden', 'frameworkid', $frameworkid);
         $mform->addElement('submit', 'downloadbutton', get_string('download', 'admin'));
 
     }
