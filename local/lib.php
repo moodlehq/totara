3e60b7df7922213c24c550262d4e3653d7cb7d41
Simon Coggins <simonc@catalyst.net.nz>
2009-12-08 13:03:24 +1300

    Modified restore course functionality to restore all courses in specific directory

--- local/lib.php
+++ local/lib.php
@@ -71,6 +71,35 @@ function local_postinst() {
     require_once($CFG->dirroot.'/course/lib.php');
     require_once($CFG->dirroot.'/backup/lib.php');
     require_once($CFG->dirroot.'/backup/restorelib.php');
+
+    $zips = get_course_backups($CFG->dirroot.'/local/defaultcoursebackups');
+
+    foreach($zips as $zip) {
+        if(file_exists($zip['filename'])) {
+            $course = new StdClass;
+            $course->fullname = $zip['fullname'];
+            $course->shortname = $zip['shortname'];
+            $course->category = 1; // Miscellaneous
+            print "Trying to create course \"{$zip['fullname']}\" ({$zip['shortname']}) ID={$zip['courseid']}\n";
+            // check if the ID we want to use is already a course
+            if(course_exists($zip['courseid'])) {
+                print "Error: Course ID already in use. Not restoring course";
+            } else {
+                if ($newcourse = create_course($course)) {
+                    // rewrite the courseid that is created to use 
+                    // the one we want. If that ID is already in use, 
+                    // prints an error and returns false
+                    //if(rewrite_course_id($newcourse->id, $zip['courseid'])) {
+                        import_backup_file_silently($zip['filename'], $zip['courseid'], true, false, array(), RESTORETO_NEW_COURSE);
+                    //}
+                }
+            }
+        }
+    }
+
+
+
+    /*
     if (file_exists($CFG->dirroot.'/local/defaultcoursebackups/inductioncourse.zip')) {
         $course = new StdClass;
         $course->fullname  = get_string('induction','local');
@@ -80,7 +109,7 @@ function local_postinst() {
         import_backup_file_silently($CFG->dirroot.'/local/defaultcoursebackups/backup-presentation_skills-20091124-1116.zip', $newcourse->id, true, false, array(), RESTORETO_NEW_COURSE);
         }
     }
-
+     */
     // set up frontpage
     set_config('frontpage', '');
     set_config('frontpageloggedin', '');
@@ -91,4 +120,74 @@ function local_postinst() {
     return true;
 
 }
+
+function rewrite_course_id($oldid, $newid) {
+    set_field('course','id',$newid,'id',$oldid);
+    set_field('course_sections','id',$newid,'id',$oldid);
+
+    return true;
+}
+
+
+function course_exists($id) {
+    if(get_record('course','id', $id)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+
+/**
+ * Given a directory, returns an array of course zip files which match
+ * the format [shortname]===[fullname]===[courseid].zip
+ * 
+ * Components are parse and returned as associative array. If no matching
+ * zip files found, returns an empty array
+ *
+ * @param string $backupdir Directory to search
+ * @return array Associative array of filename, shortname, fullname, courseid 
+**/
+function get_course_backups($backupdir) {
+
+    $ret = array();
+    if (is_dir($backupdir)) {
+        if ($dh = opendir($backupdir)) {
+            while (($file = readdir($dh)) !== false) {
+                // exclude directories
+                if (is_dir($file)) {
+                    continue;
+                }
+                // not a zip
+                if (substr($file, -4) != '.zip') {
+                    continue;
+                }
+
+                // remove .zip extension
+                $filename = substr($file, 0, -4);
+
+                // split name
+                list($shortname, $fullname, $courseid) = explode("===", $filename);
+
+                // filename isn't in expected format of:
+                // [shortname]===[fullname]===[courseid].zip
+                if(!$shortname || !$fullname || !$courseid) {
+                    continue;
+                }
+                $row = array();
+                $row['filename'] = $backupdir."/".$file;
+                $row['shortname'] = $shortname;
+                $row['fullname'] = $fullname;
+                $row['courseid'] = (int) $courseid;
+                $ret[] = $row;
+            }
+            closedir($dh);
+        }
+    }
+    return $ret;
+
+}
+
 
