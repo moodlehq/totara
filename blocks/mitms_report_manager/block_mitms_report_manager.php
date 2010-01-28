9473ec34dc0751dae1a1f3b948196555faa3b1ee
Simon Coggins <simonc@catalyst.net.nz>
2010-01-28 15:59:58 +1300

    blocks/mitms_report_manager/block_mitms_report_manager: Added capability check

--- blocks/mitms_report_manager/block_mitms_report_manager.php
+++ blocks/mitms_report_manager/block_mitms_report_manager.php
@@ -20,12 +20,23 @@ class block_mitms_report_manager extends block_base {
     }
 
     function get_content() {
+
         if ($this->content !== NULL) {
             return $this->content;
         }
 
+        if (empty($this->instance->pinned)) {
+            $context = get_context_instance(CONTEXT_BLOCK, $this->instance->id);
+        } else {
+            $context = get_context_instance(CONTEXT_SYSTEM); // pinned blocks do not have own context
+        }
+
         $this->content = new stdClass;
-        $this->content->text = mitms_print_report_manager(true);
+        if(has_capability('moodle/local:viewstaffreports',$context) || has_capability('moodle/local:viewlocalreports',$context)) {
+            $this->content->text = mitms_print_report_manager(true);
+        } else {
+            $this->content->text = '';
+        }
         $this->content->footer = '';
 
         return $this->content;
