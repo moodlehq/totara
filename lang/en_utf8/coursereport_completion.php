1cf64c7d911491768ea3b1439d75bde14e07054e
Aaron Barnes <aaronb@catalyst.net.nz>
2010-03-02 15:21:46 +1300

    Remove unneccesary lang strings

--- lang/en_utf8/coursereport_completion.php
+++ lang/en_utf8/coursereport_completion.php
@@ -1,13 +1,3 @@
 <?php
 
-$string['activitiescomplete'] = 'Activities complete';
-$string['complete'] = 'Courses completed';
-$string['completion:view'] = 'View completion report';
-$string['completiondate'] = 'Completion Date';
-$string['completions'] = 'Course completions';
-$string['detailedview'] = 'Detail view';
-$string['incomplete'] = 'Incomplete';
-$string['inprogress'] = 'Courses in progress';
-$string['nocompletioncoursesenroled'] = 'User not enrolled in any courses with completion enabled';
-$string['nocompletions'] = 'No completion information exists for this user';
-$string['unstarted'] = 'Courses not yet started';
+$string['completion:view'] = 'View course completion report';
