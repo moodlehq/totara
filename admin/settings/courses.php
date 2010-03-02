adb72732a67020e4d930d31d1c9c592e52bddc94
Aaron Barnes <aaronb@catalyst.net.nz>
2010-03-02 15:23:19 +1300

    Make "completion tracking beings on enrolment" default to true

--- admin/settings/courses.php
+++ admin/settings/courses.php
@@ -51,7 +51,7 @@ if ($hassiteconfig
         $temp->add(new admin_setting_configselect('moodlecourse/enablecompletion', get_string('completion','completion'), '',
             1, array(0 => get_string('completiondisabled','completion'), 1 => get_string('completionenabled','completion'))));
 
-        $temp->add(new admin_setting_configcheckbox('moodlecourse/completionstartonenrol', get_string('completionstartonenrol','completion'), get_string('completionstartonenrolhelp', 'completion'), 0));
+        $temp->add(new admin_setting_configcheckbox('moodlecourse/completionstartonenrol', get_string('completionstartonenrol','completion'), get_string('completionstartonenrolhelp', 'completion'), 1));
     }
 
     $ADMIN->add('courses', $temp);
