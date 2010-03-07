cde5b6404187631ffddd3d9a9fcd2a80d40932bf
Jonathan Newman <jonathan.newman@catalyst.net.nz>
2010-03-07 23:53:41 +1300

    lang/idp: add due date string

--- lang/en_utf8/idp.php
+++ lang/en_utf8/idp.php
@@ -160,6 +160,7 @@ $string['deleteplanbutton'] = 'Delete this plan';
 $string['deleteitembutton'] = 'Delete this item from the list';
 $string['disclaimer'] = 'New plan disclaimer';
 $string['dragheretoassign'] = 'Drag here to assign';
+$string['duedate'] = 'Due date';
 $string['edititembutton'] = 'Edit or rename this item';
 $string['editlatestrevision'] = 'Edit latest revision of this plan';
 $string['emailnotificationfromname'] = 'IDP Module';
