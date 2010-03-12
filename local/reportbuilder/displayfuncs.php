31830245b206afc5a98af59b2116415cf6d26c14
Simon Coggins <simonc@catalyst.net.nz>
2010-03-12 14:57:36 +1300

    local/reportbuilder: Added competency evidence admin settings column

--- local/reportbuilder/displayfuncs.php
+++ local/reportbuilder/displayfuncs.php
@@ -138,3 +138,28 @@ function reportbuilder_facetoface_status($status, $row) {
 
 }
 
+///////////////////////////////////////////
+
+// Functions for use by adminoptions.php only
+
+// Displays links to admin functions for competency evidence
+function reportbuilder_ce_admin_options($row) {
+    global $CFG;
+    $userid = $row->settings_user;
+    $ceid = $row->settings_id;
+    $addstr = get_string('addforthisuser','local');
+    $editstr = get_string('edit');
+    $deletestr = get_string('delete');
+    $addlink = '<a href="'.$CFG->wwwroot.'/hierarchy/type/competency/evidence/add.php?userid='.$userid.'&amp;s='.sesskey().
+        '&amp;returnurl='.urlencode(qualified_me()).'" title="'.$addstr.
+        '"><img src="'.$CFG->pixpath.'/t/add.gif" class="iconsmall" alt='.$addstr.'" /></a>';
+    $editlink = '<a href="'.$CFG->wwwroot.'/hierarchy/type/competency/evidence/edit.php?id='.$ceid.'&amp;s='.sesskey().
+        '&amp;returnurl='.urlencode(qualified_me()).'" title="'.$editstr.
+        '"><img src="'.$CFG->pixpath.'/t/edit.gif" class="iconsmall" alt='.$editstr.'" /></a>';
+    $deletelink = '<a href="'.$CFG->wwwroot.'/hierarchy/type/competency/evidence/delete.php?id='.$ceid.'&amp;s='.sesskey().
+        '&amp;returnurl='.urlencode(qualified_me()).'" title="'.$deletestr.
+        '"><img src="'.$CFG->pixpath.'/t/delete.gif" class="iconsmall" alt='.$deletestr.'" /></a>';
+    return $addlink.' '.$editlink .' '.$deletelink;
+}
+
+
