e45366f5f8cd64f4e777872167662a216ccd32b8
Aaron Wells <aaronw@catalyst.net.nz>
2010-04-15 16:51:04 +1200

    Correcting errors in the capability change script
    Bug #5829

--- local/db/upgrade.php
+++ local/db/upgrade.php
@@ -1332,7 +1332,6 @@ function xmldb_local_upgrade($oldversion) {
         $field = new XMLDBField('grade');
         $result = $result && drop_field($table, $field);
     }
-    return $result;
 
     // Reorganizing the IDP-related capabilities
     if ($result && $oldversion < 2010041400){
@@ -1382,12 +1381,12 @@ function xmldb_local_upgrade($oldversion) {
             // Update the role->capability mappings to use the new name
             if ( false === execute_sql("
                 update {$CFG->prefix}role_capabilities
-                set capability='{$nename}'
+                set capability='{$newname}'
                 where capability='{$oldname}'
             ") ){
                 $result = false;
             }
         }
     }
-
+    return $result;
 }
