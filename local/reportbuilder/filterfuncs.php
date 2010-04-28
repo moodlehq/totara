c7010f12c91a76a6bf16e6f4e5d50f227fe62861
Simon Coggins <simonc@catalyst.net.nz>
2010-04-29 11:09:13 +1200

    local/reportbuilder: Changed filter funcs to be none source-specific Bug #5973

--- local/reportbuilder/filterfuncs.php
+++ local/reportbuilder/filterfuncs.php
@@ -2,19 +2,13 @@
 
 // functions to return list of options for filter select elements
 // specified in filteroptions.php
-
-function get_course_categories_list() {
-    global $CFG;
-    require_once($CFG->dirroot.'/course/lib.php');
-    make_categories_list($cats, $unused);
-
-    return $cats;
-}
-
 function get_organisations_list($restrictions) {
     global $CFG,$USER;
     require_once($CFG->dirroot.'/hierarchy/lib.php');
     require_once($CFG->dirroot.'/hierarchy/type/organisation/lib.php');
+
+    //TODO fix this to use only available options
+    $restrictions = null;
 
     $baseorg = null; // default to top of tree
 
@@ -34,7 +28,7 @@ function get_organisations_list($restrictions) {
     }
     // only local restrictions set - limit pulldown options
     if($localset && !$nonlocal) {
-        $orgid = get_field('position_assignment','organisationid','userid',$USER->id);
+        $orgid = get_field('pos_assignment','organisationid','userid',$USER->id);
         // set to use users org id if it's set
         if(isset($orgid)) {
             $baseorg = $orgid;
@@ -58,7 +52,31 @@ function get_positions_list() {
 
     return $positions;
 
+}
 
+function get_proficiency_list() {
+    // use all possible scale values
+    $scale_values = get_records('comp_scale_values', '', '', 'scaleid, sortorder');
+
+    $proficiencies = array();
+    foreach($scale_values as $scale_value) {
+        $id = $scale_value->id;
+        $name = $scale_value->name;
+        $proficiencies[$id] = $name;
+    }
+
+    return $proficiencies;
+}
+
+// functions to return list of options for filter select elements
+// specified in filteroptions.php
+
+function get_course_categories_list() {
+    global $CFG;
+    require_once($CFG->dirroot.'/course/lib.php');
+    make_categories_list($cats, $unused);
+
+    return $cats;
 }
 
 function get_completion_status_list() {
@@ -68,6 +86,38 @@ function get_completion_status_list() {
     $proficiencyselect['Not Completed'] = 'Not Completed';
 
     return $proficiencyselect;
+}
+
+// functions to return list of options for filter select elements
+// specified in filteroptions.php
+
+function get_session_status_list() {
+    $status = array();
+    $status[100] = 'Fully Attended';
+    $status[90] = 'Partially Attended';
+    $status[80] = 'No Show';
+    $status[70] = 'Booked';
+    $status[60] = 'Waitlisted';
+    $status[50] = 'Approved';
+    $status[40] = 'Requested';
+    $status[30] = 'Request Denied';
+    $status[20] = 'Session Cancelled';
+    $status[10] = 'User Cancelled';
+    return $status;
+}
+
+function get_yesno_list() {
+    $yn = array();
+    $yn['Yes'] = 'Yes';
+    $yn['No'] = 'No';
+    return $yn;
+}
+
+function get_coursedelivery_list() {
+    $coursedelivery = array();
+    $coursedelivery['Internal'] = 'Internal';
+    $coursedelivery['External'] = 'External';
+    return $coursedelivery;
 }
 
 function get_scorm_attempt_list() {
@@ -102,3 +152,4 @@ function get_scorm_status_list() {
     }
     return $statusselect;
 }
+
