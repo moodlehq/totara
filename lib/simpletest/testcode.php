90a0893bf902084f7b6abe65db1f734bee197a08
Simon Coggins <simonc@catalyst.net.nz>
2010-05-24 14:32:01 +1200

    lib/simpletest/testcode: Fixed test to exclude .git and build directories Bug #6080

--- lib/simpletest/testcode.php
+++ lib/simpletest/testcode.php
@@ -17,7 +17,7 @@ class code_test extends UnitTestCase {
 
     var $badstrings;
     var $extensions_to_ignore = array('exe', 'gif', 'ico', 'jpg', 'png', 'ttf');
-    var $ignore_folders = array();
+    var $ignore_folders = array('.git','build');
 
     function test_dnc() {
         global $CFG;
@@ -30,7 +30,11 @@ class code_test extends UnitTestCase {
         foreach ($this->badstrings as $description => $ignored) {
             $this->allok[$description] = true;
         }
-        recurseFolders($CFG->dirroot, array($this, 'search_file_for_dnc'), $regexp, true);
+        $skip = array();
+        foreach ($this->ignore_folders as $ignore) {
+            $skip[] = $CFG->dirroot.'/'.$ignore;
+        }
+        recurseFolders($CFG->dirroot, array($this, 'search_file_for_dnc'), $regexp, true, $skip);
         foreach ($this->badstrings as $description => $ignored) {
             if ($this->allok[$description]) {
                 $this->pass("No files contain $description.");
