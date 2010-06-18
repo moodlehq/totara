761058be435b8f8d460926c7c3b47336e444d78c
Aaron Barnes <aaronb@catalyst.net.nz>
2010-06-18 15:25:22 +1200

    T-6553 lib/tcpdf: updated tcpdf to version 4.9.005

--- lib/tcpdf/config/tcpdf_config.php
+++ lib/tcpdf/config/tcpdf_config.php
@@ -2,32 +2,31 @@
 //============================================================+
 // File name   : tcpdf_config.php
 // Begin       : 2004-06-11
-// Last Update : 2006-05-18
+// Last Update : 2010-04-28
 //
-// Description : Congiguration file for TCPDF.
+// Description : Configuration file for TCPDF.
 //
 // Author: Nicola Asuni
 //
 // (c) Copyright:
-//               Tecnick.com S.r.l.
-//               Via Ugo Foscolo n.19
-//               09045 Quartu Sant'Elena (CA)
+//               Nicola Asuni
+//               Tecnick.com s.r.l.
+//               Via Della Pace, 11
+//               09044 Quartucciu (CA)
 //               ITALY
 //               www.tecnick.com
 //               info@tecnick.com
 //============================================================+
 
 /**
- * Congiguration file for TCPDF.
+ * Configuration file for TCPDF.
  * @author Nicola Asuni
- * @copyright Copyright &copy; 2004, Tecnick.com S.r.l. - Via Ugo Foscolo
- *            n.19 - 09045 Quartu Sant'Elena (CA) - ITALY - www.tecnick.com -
- *            info@tecnick.com
+ * @copyright 2004-2010 Nicola Asuni - Tecnick.com S.r.l (www.tecnick.com) Via Della Pace, 11 - 09044 - Quartucciu (CA) - ITALY - www.tecnick.com - info@tecnick.com
  * @package com.tecnick.tcpdf
- * @version 1.53.0.TC017
+ * @version 4.9.005
  * @link http://tcpdf.sourceforge.net
  * @license http://www.gnu.org/copyleft/lesser.html LGPL
- * @since 2004-06-11
+ * @since 2004-10-27
  *
  * Moodle-specific changes:
  *   Default values for K_PATH_MAIN and K_PATH_URL are automatically set for
