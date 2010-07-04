8c1595bbb0b067b12a60adc65e844c88a8a93079
Peter Bulmer <peter.bulmer@catalyst.net.nz>
2010-07-04 22:11:34 +1200

    mitms/guides - add mitms-specific steps & config

---
+++ guides/guidedata/Configure Hierarchies.php
@@ -0,0 +1,7 @@
+<?php
+# Raw (not database escaped) strings go here.
+# Name will be determined by filename, must start with a letter, contain only letters, numbers, underscores, spaces and hyphens.
+# Name must be unique to a guide
+$guide->description = 'A high-level guide to assigning users to hierarchies in a new MITMS';
+$guide->steps = 'mitms_assign_user_manager_role,mitms_assign_user_positions,mitms_assign_user_organisations,mitms_assign_users_manager';
+?>
