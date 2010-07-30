3e489e6782e196e8094a25c11dda95043143ff7e
Simon Coggins <simonc@catalyst.net.nz>
2010-07-30 17:57:05 +1200

    admin/settings: Fixed capabilities on hierarchy admin menu items

--- admin/settings/competencies.php
+++ admin/settings/competencies.php
@@ -3,7 +3,7 @@
 // This file defines settingpages and externalpages under the "competency" category
 
     $ADMIN->add('competencies', new admin_externalpage('competencyframeworkmanage', get_string('competencyframeworkmanage', 'competency'), "$CFG->wwwroot/hierarchy/framework/index.php?type=competency",
-            array('moodle/local:viewcompetency')));
+            array('moodle/local:updatecompetencyframeworks')));
 
     $ADMIN->add('competencies', new admin_externalpage('competencymanage', get_string('competencymanage', 'competency'), "$CFG->wwwroot/hierarchy/index.php?type=competency",
-            array('moodle/local:viewcompetency')));
+            array('moodle/local:updatecompetency')));
--- admin/settings/positions.php
+++ admin/settings/positions.php
@@ -3,7 +3,7 @@
 // This file defines settingpages and externalpages under the "positions" category
 
     $ADMIN->add('positions', new admin_externalpage('positionframeworkmanage', get_string('positionframeworkmanage', 'position'), "$CFG->wwwroot/hierarchy/framework/index.php?type=position",
-            array('moodle/local:viewposition')));
+            array('moodle/local:updatepositionframeworks')));
 
     $ADMIN->add('positions', new admin_externalpage('positionmanage', get_string('positionmanage', 'position'), $CFG->wwwroot . '/hierarchy/index.php?type=position',
             array('moodle/local:updateposition')));
