7fa94bbf691dee707de5e65cf4cfc3dfb9c4f77d
Eugene Venter <eugene@catalyst.net.nz>
2010-08-10 16:05:32 +1200

    local/reportbuilder/showhide.js.php: Added OK button; changed the handler

--- local/reportbuilder/showhide.js.php
+++ local/reportbuilder/showhide.js.php
@@ -13,14 +13,19 @@ $(function() {
         $('#show-showhide-dialog').css('display','block');
         var url = '<?php echo $CFG->wwwroot ?>/local/reportbuilder/';
 
-        var handler = new mitmsDialog_handler_treeview();
+        var handler = new mitmsDialog_handler();
         var name = 'showhide';
 
         mitmsDialogs[name] = new mitmsDialog(
             name,
             'show-'+name+'-dialog',
-            {},
-            url+'showhide.php?id='+id
+            {
+                buttons: {
+                    'Ok': function() { handler._cancel() }
+                }
+            },
+            url+'showhide.php?id='+id.toString(),
+            handler
         );
     })();
 
