9ba3aaf9a3b4b1d370d459e2cc62b8e76ce028b9
Simon Coggins <simonc@catalyst.net.nz>
2010-08-24 09:59:17 +1200

    T-5734 reportbuilder: Fixed notice in include_js function

--- local/reportbuilder/lib.php
+++ local/reportbuilder/lib.php
@@ -235,7 +235,9 @@ class reportbuilder {
 
 
         local_js($code);
-        require_js(array_values($js));
+        if(count($js)) {
+            require_js(array_values($js));
+        }
 
         if($dialog) {
             print '
