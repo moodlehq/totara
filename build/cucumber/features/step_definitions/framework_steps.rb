67bcd6aa37d2c634155fef8f86e09d8028a703e0
Simon Coggins <simonc@catalyst.net.nz>
2010-09-08 11:57:02 +1200

    T-6691 cucumber: Added step to generate complete scale

--- build/cucumber/features/step_definitions/framework_steps.rb
+++ build/cucumber/features/step_definitions/framework_steps.rb
@@ -3,3 +3,17 @@ Given /^there is a competency scale$/ do
   Given "there is 1 competency scale record"
     And "there are 3 competency scale value records"
 end
+
+Given /^there is a complete competency scale$/ do
+  Given "the competency scale table contains:", table(%{
+    |name|
+    |Complete Competency Scale|
+})
+  newscaleid = get_scale_id('Complete Competency Scale')
+  And "the competency scale value table contains:", table(%{
+    |scaleid|
+    |#{newscaleid}|
+    |#{newscaleid}|
+    |#{newscaleid}|
+})
+end
