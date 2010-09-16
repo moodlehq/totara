2a31a7aa0fe2dc260eb8ce83ae43b2d0cdc414e3
Alastair Munro <alastair.munro@catalyst.net.nz>
2010-09-17 10:48:28 +1200

    T-6722 customfield/file/field.class.php: add custom disable method for filepicker

--- customfield/field/file/field.class.php
+++ customfield/field/file/field.class.php
@@ -10,5 +10,16 @@ class customfield_file extends customfield_base {
         $mform->addElement('choosecoursefileorimsrepo', $this->inputname, format_string($this->field->fullname));
         $mform->setType($this->inputname, PARAM_RAW);  // We need to find a better PARAM
     }
+
+    function edit_field_set_locked(&$mform) {
+        if (!$mform->elementExists($this->inputname)) {
+            return;
+        }
+        if ($this->is_locked()) {
+            $mform->hardFreeze($this->inputname);
+            $mform->disabledif($this->inputname, 1);
+            $mform->setConstant($this->inputname, $this->data);
+        }
+    }
 }
 ?>
