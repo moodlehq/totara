dab33d940dbbf4ef3d443ab2a05eba54ff7553a3
Aaron Barnes <aaronb@catalyst.net.nz>
2010-09-30 12:19:34 +1300

    Revert "T-6704: hierarchy/item/view.php: Simply some code"
    This reverts commit d8067b4d8b90a569cec2cff2ab99bf7e1fb442b2.

--- hierarchy/item/view.php
+++ hierarchy/item/view.php
@@ -59,7 +59,14 @@ $can_edit = has_capability('moodle/local:update'.$type, $sitecontext);
 
 // Run any hierarchy type specific code
 if ($editingon) {
-    $hierarchy->hierarchy_page_setup('item/view', $item);
+    $compfw = optional_param('framework', get_field_sql("SELECT id FROM {$CFG->prefix}comp_framework WHERE visible=1 ORDER BY sortorder ASC"), PARAM_INT);
+    $setupitem = new stdClass;
+    $setupitem->id = $item->id;
+    $setupitem->frameworkid = $compfw;
+
+    $hierarchy->hierarchy_page_setup('item/view', $setupitem);
+
+    unset($setupitem);
 }
 
 // Display page header
