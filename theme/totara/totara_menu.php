eede86539f36651f464b3a6f5fe9162f888ce4c8
Alastair Munro <alastair.munro@catalyst.net.nz>
2010-10-08 10:13:42 +1300

    T-6802 theme/totara: remove my team menu option when a user doesnt have a team

--- theme/totara/totara_menu.php
+++ theme/totara/totara_menu.php
@@ -7,93 +7,87 @@
 </form>
 </div>
 
-
 <ul>
-     
-        <li id="menu1" class="first"><div><a href="<?php echo $CFG->wwwroot.'/index.php' ?>">Home</a>
-					
-        <ul>
-        <?php
+    <li id="menu1" class="first">
+        <div><a href="<?php echo $CFG->wwwroot.'/index.php' ?>">Home</a>
+	        <ul>
+                <?php
+                $text ='<li class="first"><a href="'.$CFG->wwwroot . '/user/view.php?id=' . $USER->id . '">' . get_string('myprofile', 'local') . '</a></li>';
+                echo $text;
+                ?>
+            </ul>
+        </div>
+    </li>
 
- $text ='<li class="first"><a href="'.$CFG->wwwroot . '/user/view.php?id=' . $USER->id . '">' . get_string('myprofile', 'local') . '</a></li>';
- 
- echo $text;
-?>
 
-           </ul></div>
-        </li>
-        <li id="menu2"><div><a href="<?php echo $CFG->wwwroot.'/idp/index.php' ?>"><?php echo get_string('mylearning', 'local') ?></a>
-					
-        <ul>
-        <?php
+    <li id="menu2">
+        <div><a href="<?php echo $CFG->wwwroot.'/idp/index.php' ?>"><?php echo get_string('mylearning', 'local') ?></a>
+            <ul>
+            <?php
+                 $text ='<li><a href="'.$CFG->wwwroot.'/idp/index.php">'.get_string('mydevelopmentplans', 'local').'</a></li>';
+                 $text .='<li><a href="'.$CFG->wwwroot.'/my/bookings.php">'.get_string('mybookings', 'local').'</a></li>';
+                 $text .='<li class="last"><a href="'.$CFG->wwwroot.'/my/records.php">'.get_string('myrecordoflearning', 'local').'</a></li>';
+                 echo $text;
+            ?>
+            </ul>
+        </div>
+    </li>
 
- $text ='<li><a href="'.$CFG->wwwroot.'/idp/index.php">'.get_string('mydevelopmentplans', 'local').'</a></li>';
- $text .='<li><a href="'.$CFG->wwwroot.'/my/bookings.php">'.get_string('mybookings', 'local').'</a></li>';
- $text .='<li class="last"><a href="'.$CFG->wwwroot.'/my/records.php">'.get_string('myrecordoflearning', 'local').'</a></li>';
- 
- echo $text;
-?>
 
-           </ul></div>
-        </li>
-        <li id="menu3"><div><a href="<?php echo $CFG->wwwroot.'/my/team.php' ?>"><?php echo get_string('myteam', 'local') ?></a>
-					
-        <ul>
-        <?php
+    <?php if($staff = totara_get_staff()) { ?>
+    <li id="menu3">
+        <div>
+            <a href="<?php echo $CFG->wwwroot.'/my/team.php' ?>"><?php echo get_string('myteam', 'local') ?></a>
+			<ul>
+	        </ul>
+        </div>
+    </li>
+    <?php } ?>
 
- $text ='<li class="first"><a href="">Item One</a></li>';
- $text .='<li><a href="">Item Two</a></li>';
- $text .='<li><a href="">Item Three</a></li>';
- $text .='<li><a href="">Item Four</a></li>';
- $text .='<li class="last"><a href="">Item Five</a></li>';
- 
-?>
-	
 
-           </ul></div>
-        </li>
-        <li id="menu4"><div><a href="<?php echo $CFG->wwwroot.'/my/reports.php' ?>"><?php echo get_string('myreports', 'local') ?></a>
-					
-        <ul>
-        <?php
+    <li id="menu4">
+        <?php if($staff) {
+            echo '<div>';
+        }
+        else {
+            echo '<div class=noteam>';
+        } ?>
+            <a href="<?php echo $CFG->wwwroot.'/my/reports.php' ?>"><?php echo get_string('myreports', 'local') ?></a>
+            <ul>
+            </ul>
+        </div>
+    </li>
 
- $text ='<li class="first"><a href="">Item One</a></li>';
- $text .='<li><a href="">Item Two</a></li>';
- $text .='<li><a href="">Item Three</a></li>';
- $text .='<li><a href="">Item Four</a></li>';
- $text .='<li class="last"><a href="">Item Five</a></li>';
- 
-// echo $text;
-?>
-           </ul></div>
-        </li>
-        <li id="menu5"><div><a href="<?php echo $CFG->wwwroot.'/course/find.php' ?>"><?php echo get_string('findcourses', 'local') ?></a>
-					
-        <ul>
-        <?php
 
- $text ='<li class="first"><a href="'.$CFG->wwwroot.'/course/find.php">'.get_string('searchcourses', 'local').'</a></li>';
- $text .='<li class="last"><a href="'.$CFG->wwwroot.'/course/index.php">'.get_string('browsecategories', 'local').'</a></li>';
- 
- echo $text;
-?> 			
+    <li id="menu5">
+        <?php if($staff) {
+            echo '<div>';
+        }
+        else {
+            echo '<div class=noteam>';
+        } ?>
+        <a href="<?php echo $CFG->wwwroot.'/course/find.php' ?>"><?php echo get_string('findcourses', 'local') ?></a>
+		    <ul>
+            <?php
+                 $text ='<li class="first"><a href="'.$CFG->wwwroot.'/course/find.php">'.get_string('searchcourses', 'local').'</a></li>';
+                 $text .='<li class="last"><a href="'.$CFG->wwwroot.'/course/index.php">'.get_string('browsecategories', 'local').'</a></li>';
+                 echo $text;
+            ?>
+           </ul>
+        </div>
+    </li>
 
-           </ul></div>
-        </li>
-        <li id="menu6" class="last"><div><a href="<?php echo $CFG->wwwroot.'/blocks/facetoface/calendar.php' ?>">Calendar</a>
-					
-        <ul>
-        <?php
 
- $text ='<li class="first"><a href="">Item One</a></li>';
- $text .='<li><a href="">Item Two</a></li>';
- $text .='<li><a href="">Item Three</a></li>';
- $text .='<li><a href="">Item Four</a></li>';
- $text .='<li class="last"><a href="">Item Five</a></li>';
- 
-// echo $text;
-?> 			
-
-           </ul></div>
-        </li>
+    <li id="menu6" class="last">
+        <?php if($staff) {
+            echo '<div>';
+        }
+        else {
+            echo '<div class=noteam>';
+        } ?>
+            <a href="<?php echo $CFG->wwwroot.'/blocks/facetoface/calendar.php' ?>">Calendar</a>
+            <ul>
+            </ul>
+        </div>
+    </li>
 </ul>
