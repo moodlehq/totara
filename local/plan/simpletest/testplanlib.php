b9f5ff0ad27157704d170bb2e373e933b56629be
Aaron Barnes <aaronb@catalyst.net.nz>
2010-11-30 15:24:40 +1300

    T-6072 local/plan/simpletest: Fix name of simpletest class

--- local/plan/simpletest/testplanlib.php
+++ local/plan/simpletest/testplanlib.php
@@ -13,7 +13,7 @@ if (!defined('MOODLE_INTERNAL')) {
 require_once($CFG->dirroot . '/local/plan/lib.php');
 require_once($CFG->libdir . '/simpletestlib.php');
 
-class reportbuilderlib_test extends prefix_changing_test_case {
+class testplanlib_test extends prefix_changing_test_case {
     // test data for database
 
     // reduced version of user table
