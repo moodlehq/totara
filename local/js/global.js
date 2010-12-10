f7a75359566d6d39e9bfeb7f964f402505862ce1
Aaron Barnes <aaronb@catalyst.net.nz>
2010-12-10 13:19:49 +1300

    T-7092: Add jquery and global js script to every page load

---
+++ local/js/global.js
@@ -0,0 +1,3 @@
+/**
+ * Loaded every page
+ */
--- theme/totara/meta.php
+++ theme/totara/meta.php
@@ -1,3 +1,14 @@
 <!--[if IE 6]>
     <link rel="stylesheet" type="text/css" href="<?php echo $CFG->httpsthemewww ?>/totara/styles_ie6.css" />
 <![endif]-->
+
+<?php
+
+    require_js(
+        array(
+            $CFG->wwwroot.'/local/js/lib/jquery-1.3.2.min.js',
+            $CFG->wwwroot.'/local/js/global.js',
+        )
+    );
+
+?>
