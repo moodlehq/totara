9a61b15751ee3f891b2cd516be844944a11a8b3c
Simon Coggins <simonc@catalyst.net.nz>
2010-12-17 14:38:24 +1300

    T-7024 theme/totara: Cleaned up totara navigation tab

--- theme/totara/totara_menu.php
+++ theme/totara/totara_menu.php
@@ -1,3 +1,81 @@
+<?php
+
+// multi-dimensional array containing nested list
+// of tab names
+$navstructure = array(
+    'home' => array(
+        'profile',
+    ),
+    'mylearning' => array(
+        'learnerdashboard',
+        'learningplans',
+        'mybookings',
+        'recordoflearning',
+    ),
+    'myteam' => array(
+        'managerdashboard',
+        'teammembers',
+    ),
+    'myreports' => array(),
+    'findcourses' => array(
+        'searchcourses',
+        'browsecourses',
+    ),
+    'calendar' => array(),
+);
+
+// define partial urls which match particular tabs
+// partial matches against the start of the URL accepted
+// so '/course/' will match any page in the course directory
+// for multiple matches, use an array
+$navmatches = array(
+    'home' => '/index.php',
+    'profile' => '/user/view.php',
+    'learnerdashboard' => '/my/learning.php',
+    'learningplans' => '/local/plan/',
+    'mybookings' => array(
+        '/my/bookings.php',
+        '/my/pastbookings.php',
+    ),
+    'recordoflearning' => '/local/plan/record/',
+    'managerdashboard' => '/my/team.php',
+    'teammembers' => '/my/teammembers.php',
+    'myreports' => '/my/reports.php',
+    'searchcourses' => '/course/find.php',
+    'browsecourses' => '/course/index.php',
+    'calendar' => array(
+        '/blocks/facetoface/calendar.php',
+        '/calendar/',
+    ),
+);
+
+// Figure out which tabs should be selected for this page
+list($primary_selected, $secondary_selected) =
+   totara_get_selected_navs($navstructure, $navmatches);
+
+// add special-case overrides here
+
+// match the homepage if url is empty
+if(me() == '/') {
+    $primary_selected = 'home';
+    $secondary_selected = null;
+}
+
+// if we are viewing a learning plan, bookings,
+// or record of learning, see if we are viewing our
+// own or someone else's
+$userid = optional_param('userid', null, PARAM_INT);
+if(in_array($secondary_selected, array(
+    'learningplans', 'mybookings', 'recordoflearning'))) {
+    if(isset($userid) && $userid != $USER->id) {
+        $primary_selected = 'myteam';
+        $secondary_selected = null;
+    }
+}
+
+// get an array of class string snippets to be applied to each tab element
+$selected = totara_get_nav_select_classes($navstructure, $primary_selected, $secondary_selected);
+?>
 <div id="header-search">
 <form id="coursesearch" action="<?php echo $CFG->wwwroot ?>/course/search.php" method="get">
   <fieldset class="coursesearchbox invisiblefieldset">
@@ -8,92 +86,185 @@
 </div>
 
 <ul>
-    <li id="menu1" class="first">
-        <div><a href="<?php echo $CFG->wwwroot.'/index.php' ?>">Home</a>
-	        <ul>
-                <?php
-                $text ='<li class="last"><a href="'.$CFG->wwwroot . '/user/view.php?id=' . $USER->id . '">' . get_string('myprofile', 'local') . '</a></li>';
+    <li id="menu1" class="first<?php echo $selected['home']; ?>">
+    <div><a href="<?php echo $CFG->wwwroot.'/index.php' ?>"><?php echo get_string('home') ?></a>
+            <?php
+            if($selected['home']) {
+                $text ='<ul><li class="first last' .
+                    $selected['profile'] . '"><a href="' . $CFG->wwwroot .
+                    '/user/view.php?id=' . $USER->id . '">' .
+                    get_string('myprofile', 'local') . '</a></li></ul>';
                 echo $text;
-                ?>
-            </ul>
+            }
+            ?>
         </div>
     </li>
 
 
-    <li id="menu2">
+    <li id="menu2" class="<?php echo $selected['mylearning']; ?>">
         <div><a href="<?php echo $CFG->wwwroot.'/my/learning.php' ?>"><?php echo get_string('mylearning', 'local') ?></a>
-            <ul>
             <?php
-                 $text ='<li><a href="'.$CFG->wwwroot.'/my/learning.php">'.get_string('dashboard', 'local_dashboard').'</a></li>';
-                 $text .='<li><a href="'.$CFG->wwwroot.'/local/plan/index.php">'.get_string('learningplans', 'local').'</a></li>';
-                 $text .='<li><a href="'.$CFG->wwwroot.'/my/bookings.php">'.get_string('mybookings', 'local').'</a></li>';
-                 $text .='<li class="last"><a href="'.$CFG->wwwroot.'/local/plan/record/courses.php">'.get_string('recordoflearning', 'local').'</a></li>';
-                 echo $text;
+            if($selected['mylearning']) {
+                $text ='<ul><li class="first' . $selected['learnerdashboard'] .
+                    '"><a href="' . $CFG->wwwroot . '/my/learning.php">' .
+                    get_string('dashboard', 'local_dashboard').'</a></li>';
+                $text .='<li class="' . $selected['learningplans'] .
+                    '"><a href="' . $CFG->wwwroot . '/local/plan/index.php">' .
+                    get_string('learningplans', 'local').'</a></li>';
+                $text .='<li class="' . $selected['mybookings'] .
+                    '"><a href="' . $CFG->wwwroot . '/my/bookings.php">' .
+                    get_string('mybookings', 'local').'</a></li>';
+                $text .='<li class="last' . $selected['recordoflearning'] .
+                    '"><a href="' . $CFG->wwwroot .
+                    '/local/plan/record/courses.php">' .
+                    get_string('recordoflearning', 'local').'</a></li>';
+                $text .= '</ul>';
+                echo $text;
+            }
             ?>
-            </ul>
         </div>
     </li>
 
 
     <?php if($staff = totara_get_staff()) { ?>
-    <li id="menu3">
+    <li id="menu3" class="<?php echo $selected['myteam']; ?>">
         <div>
             <a href="<?php echo $CFG->wwwroot.'/my/team.php' ?>"><?php echo get_string('myteam', 'local') ?></a>
-			<ul>
             <?php
-                 $text ='<li><a href="'.$CFG->wwwroot.'/my/team.php">'.get_string('dashboard', 'local_dashboard').'</a></li>';
-                 $text .='<li class="last"><a href="'.$CFG->wwwroot.'/my/teammembers.php">'.get_string('teammembers', 'local').'</a></li>';
-                 echo $text;
+            if($selected['myteam']) {
+                $text ='<ul><li class="first' . $selected['managerdashboard'] .
+                    '"><a href="' . $CFG->wwwroot . '/my/team.php">' .
+                    get_string('dashboard', 'local_dashboard') . '</a></li>';
+                $text .='<li class="last' . $selected['teammembers'] .
+                    '"><a href="' . $CFG->wwwroot . '/my/teammembers.php">' .
+                    get_string('teammembers', 'local') . '</a></li></ul>';
+                echo $text;
+            }
             ?>
-	        </ul>
         </div>
     </li>
     <?php } ?>
 
-
-    <li id="menu4">
-        <?php if($staff) {
-            echo '<div>';
-        }
-        else {
-            echo '<div class=noteam>';
-        } ?>
+    <li id="menu4" class="<?php echo $selected['myreports']; ?>">
+        <div>
             <a href="<?php echo $CFG->wwwroot.'/my/reports.php' ?>"><?php echo get_string('myreports', 'local') ?></a>
-            <ul>
-            </ul>
         </div>
     </li>
 
 
-    <li id="menu5">
-        <?php if($staff) {
-            echo '<div>';
-        }
-        else {
-            echo '<div class=noteam>';
-        } ?>
+    <li id="menu5" class="<?php echo $selected['findcourses']; ?>">
+        <div>
         <a href="<?php echo $CFG->wwwroot.'/course/find.php' ?>"><?php echo get_string('findcourses', 'local') ?></a>
-		    <ul>
             <?php
-                 $text ='<li class="first"><a href="'.$CFG->wwwroot.'/course/find.php">'.get_string('searchcourses', 'local').'</a></li>';
-                 $text .='<li class="last"><a href="'.$CFG->wwwroot.'/course/index.php">'.get_string('browsecategories', 'local').'</a></li>';
-                 echo $text;
+            if($selected['findcourses']) {
+                $text ='<ul><li class="first' . $selected['searchcourses'] .
+                    '"><a href="' . $CFG->wwwroot . '/course/find.php">' .
+                    get_string('searchcourses', 'local') . '</a></li>';
+                $text .= '<li class="last' .  $selected['browsecourses'] .
+                    '"><a href="' . $CFG->wwwroot . '/course/index.php">' .
+                    get_string('browsecategories', 'local') . '</a></li>';
+                $text .= '</ul>';
+                echo $text;
+            }
             ?>
-           </ul>
         </div>
     </li>
 
 
-    <li id="menu6" class="last">
-        <?php if($staff) {
-            echo '<div>';
-        }
-        else {
-            echo '<div class=noteam>';
-        } ?>
+    <li id="menu6" class="last<?php echo $selected['calendar']; ?>">
+        <div>
             <a href="<?php echo $CFG->wwwroot.'/blocks/facetoface/calendar.php' ?>">Calendar</a>
-            <ul>
-            </ul>
         </div>
     </li>
 </ul>
+
+<?php
+
+// helper functions
+
+/**
+ * Loops through the navigation options and returns an array of classes
+ *
+ * The array contains the navigation option name as a key, and a string
+ * to be inserted into a class as the value. The string is either
+ * ' selected' if the option is currently selected, or an empty string ('')
+ *
+ * @param array $navstructure A nested array containing the structure of the menu
+ * @param string $primary_selected The name of the primary option
+ * @param string $secondary_selected The name of the secondary option
+ *
+ * @return array Array of strings, keyed on option names
+ */
+function totara_get_nav_select_classes($navstructure, $primary_selected, $secondary_selected) {
+
+    $selectedstr = ' selected';
+    $selected = array();
+    foreach($navstructure as $primary => $secondaries) {
+        if($primary_selected == $primary) {
+            $selected[$primary] = $selectedstr;
+        } else {
+            $selected[$primary] = '';
+        }
+        foreach($secondaries as $secondary) {
+            if($secondary_selected == $secondary) {
+                $selected[$secondary] = $selectedstr;
+            } else {
+                $selected[$secondary] = '';
+            }
+        }
+    }
+    return $selected;
+}
+
+
+/**
+ * Returns an array of class strings to add to each navigation tab
+ *
+ * Strings are empty unless the page should be shown as 'selected'. Keys
+ * are the tab names from $navstructure array
+ *
+ * @param array $navstructure Multi-dimensional array of the tab structure
+ * @param array $navmatches URL matches for each tab name
+ *
+ * @return array Array of class strings to add to menu items
+ */
+function totara_get_selected_navs($navstructure, $navmatches) {
+
+    $selected = null;
+    foreach ($navmatches as $pagename => $partialurls) {
+        if(is_array($partialurls)) {
+            foreach($partialurls as $partialurl) {
+                if(strncmp(me(), $partialurl,
+                    strlen($partialurl)) == 0) {
+                        $selected = $pagename;
+                    }
+            }
+        } else {
+            if(strncmp(me(), $partialurls,
+                strlen($partialurls)) == 0) {
+                    $selected = $pagename;
+                }
+        }
+    }
+
+    // now work out if any primary items should be selected
+    $primary_selected = null;
+    foreach($navstructure as $primary => $secondaries) {
+        // this is a primary item
+        if($selected == $primary) {
+            $primary_selected = $primary;
+            $secondary_selected = null;
+        }
+        // this is a secondary item, find which primary
+        // item it belongs to
+        if(in_array($selected, $secondaries)) {
+            $primary_selected = $primary;
+            $secondary_selected = $selected;
+        }
+        // otherwise, none set
+    }
+    return array($primary_selected, $secondary_selected);
+}
+
+
+?>
