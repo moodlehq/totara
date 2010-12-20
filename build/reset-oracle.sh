c456b50407bca47b990680a10854eb2b22648642
Aaron Barnes <aaronb@catalyst.net.nz>
2010-12-20 17:20:01 +1300

    T-6080 build/reset-oracle.sh: Add oracle reset script

---
+++ build/reset-oracle.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+# .git/hooks/post-checkout must by symbolicly linked to this file
+
+# Warning! It appears when an update to this file is checked out,
+# the hook runs the previous version. So it takes an extra build
+# to run the new reset file
+
+echo "Update hudson directory permissions"
+# when hudson updates it resets to 750 which, makes the webroot
+# inaccessible. Resetting every build is overkill but does the
+# job
+chmod 755 /var/lib/hudson
+
+echo "Delete config.php";
+rm config.php
+
+echo "Drop tables from database hudson/moodle";
+DROPDB="DROP database \`t1-hudsontesting\`;"
+mysql -u hudson -e "$DROPDB"
+
+echo "Delete old moodledata";
+rm -Rf ../moodledata/
+
+echo "Re-create moodledata";
+mkdir ../moodledata
+chmod 777 ../moodledata
+
+echo "Reset apache logs";
+rm ../moodle_error.log
+touch ../moodle_error.log
+chmod 777 ../moodle_error.log
+
+echo "Initialize installation";
+/usr/bin/php admin/cliupgrade.php \
+      --lang=en_utf8 \
+      --webaddr="http://brumbies.wgtn.cat-it.co.nz/totara-oracle-hudson" \
+      --moodledir="/var/lib/hudson/jobs/Totara-Oracle/workspace" \
+      --datadir="/var/lib/hudson/jobs/Totara-Oracle/moodledata" \
+      --dbtype="oci8po" \
+      --dbname="127.0.0.1/moodle" \
+      --dbhost="localhost" \
+      --dbuser="hudson" \
+      --dbpass="password" \
+      --prefix="m_" \
+      --verbose=3 \
+      --sitefullname="Totara" \
+      --siteshortname="Totara" \
+      --sitesummary="" \
+      --sitenewsitems=0 \
+      --adminfirstname=Admin \
+      --adminlastname=User \
+      --adminemail=simonc@catalyst.net.nz \
+      --adminusername=admin \
+      --adminpassword="passworD1!" \
+      --interactivelevel=0
+
+echo "Generate some test users"
+php -f build/generate-users.php
+
+echo "Hit notifications page to complete installation";
+wget -O - http://brumbies.wgtn.cat-it.co.nz/totara-oracle-hudson/admin/index.php
+
