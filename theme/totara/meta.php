20a445baecf3f31f226d2ad2dff408f11e38ad86
Jake Salmon <jake.salmon@kineo.com>
2010-12-21 13:03:29 +0000

    T-7075 /theme/totara: Changed the totara styled buttons to use CSS rather than JavaScript

--- theme/totara/meta.php
+++ theme/totara/meta.php
@@ -1,6 +1,9 @@
 <!--[if IE 6]>
     <link rel="stylesheet" type="text/css" href="<?php echo $CFG->httpsthemewww ?>/totara/styles_ie6.css" />
 <![endif]-->
+<!--[if IE 7]>
+    <link rel="stylesheet" type="text/css" href="<?php echo $CFG->httpsthemewww ?>/totara/styles_ie7.css" />
+<![endif]-->
 
 <?php
 
