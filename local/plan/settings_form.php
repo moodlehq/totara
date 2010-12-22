aaff6895b86be3fd791431de40ddfccc37bbf331
Alastair Munro <alastair@catalyst.net.nz>
2010-12-22 16:55:44 +1300

    T-7174 local/plan/settings_form.php: fix default values for plan permissions

--- local/plan/settings_form.php
+++ local/plan/settings_form.php
@@ -37,7 +37,7 @@ function development_plan_build_settings_form(&$mform, $id) {
 
     foreach(development_plan::$permissions as $action => $requestable) {
         foreach($DP_AVAILABLE_ROLES as $role){
-            $sql = "SELECT value FROM {$CFG->prefix}dp_permissions WHERE role='$role' AND component='plan' AND action='{$action}'";
+            $sql = "SELECT value FROM {$CFG->prefix}dp_permissions WHERE role='$role' AND component='plan' AND action='{$action}' AND templateid='{$id}'";
             $defaultvalue = get_field_sql($sql);
             $mform->setDefault($action.$role, $defaultvalue);
         }
