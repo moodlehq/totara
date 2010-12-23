d07d1f98e7d3b9aaebfcfe5bcbbd4b1a89e69e1a
Aaron Barnes <aaronb@catalyst.net.nz>
2010-12-23 17:16:25 +1300

    T-6080 lib/simpletestlib.php: Make sure SQL errors parse

--- lib/simpletestlib.php
+++ lib/simpletestlib.php
@@ -414,10 +414,12 @@ function _private_execute_sql($sql, $localdb = null, $ignoreerror = false) {
                 return false;
             }
 
+            echo '<exception>';
             echo 'SQL ERROR: ';
             echo htmlspecialchars($localdb->ErrorMsg());
             echo ' STATEMENT: ';
             echo htmlspecialchars($sql);
+            echo '</exception>';
         }
         else {
             echo '<p>SQL ERROR: ', $localdb->ErrorMsg(), ". STATEMENT: $sql</p>";
