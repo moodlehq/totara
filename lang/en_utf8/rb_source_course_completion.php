48574138e3b0b7b7388a3002a48bdd57693a3c16
Chris Wharton <chrisw@catalyst.net.nz>
2011-01-11 11:08:29 +1300

    T-6862 lang/en_utf8/rb_source_course_completion.php: added references to lang strings local/rb_sources/rb_source_course_completion.php: created lang file

---
+++ lang/en_utf8/rb_source_course_completion.php
@@ -0,0 +1,31 @@
+<?php
+// lang string for local/rb_sources/rb_source_course_completion.php
+// columns
+$string['completionstatus'] = 'Completion Status';
+$string['completiondate'] = 'Completion Date';
+$string['datestarted'] = 'Date Started';
+$string['dateenrolled'] = 'Date Enrolled';
+$string['completionorgid'] = 'Completion Organisation ID';
+$string['completionorgpath'] = 'Completion Organisation Path';
+$string['completionorgname'] = 'Completion Organisation Name';
+$string['completionposid'] = 'Completion Position ID';
+$string['completionpospath'] = 'Completion Position Path';
+$string['completionposname'] = 'Completion Position Name';
+$string['grade'] = 'Grade';
+$string['passgrade'] = 'Pass Grade';
+$string['requiredgrade'] = 'Grade and required grade';
+// filters
+$string['datecompleted'] = 'Date Completed';
+$string['datestarted'] = 'Date Started';
+$string['dateenrolled'] = 'Date Enrolled';
+$string['completionstatus'] = 'Completion Status';
+$string['officewhencompletedbasic'] = 'Office when completed (basic)';
+$string['orgwhencompleted'] = 'Organisation when completed';
+$string['poswhencompletedbasic'] = 'Position when completed (basic)';
+$string['poswhencompleted'] = 'Position when completed';
+// content
+$string['currentorg'] = 'The user\'s current organisation';
+$string['currentpos'] = 'The user\'s current position';
+$string['orgwhencompleted'] = 'The organisation when completed';
+$string['user'] = 'The user';
+$string['completiondate'] = 'The completion date';
