1215fe25ba8925fd68d5e5808c580d9a7288c8ed
Chris Wharton <chrisw@catalyst.net.nz>
2011-01-11 14:57:48 +1300

    T-6862 local/rb_sources/rb_source_site_logs.php: added lang file references lang/en_utf8/rb_source_site_logs.php: created lang file

---
+++ lang/en_utf8/rb_source_site_logs.php
@@ -0,0 +1,16 @@
+<?php
+// lang file for local/rb_sources/rb_source_site_logs.php
+// columns
+$string['time'] = 'Time';
+$string['ip'] = 'IP address';
+$string['module'] = 'Module';
+$string['cmid'] = 'CMID';
+$string['action'] = 'Action';
+$string['actionlink'] = 'Action (linked to url)';
+$string['url'] = 'URL';
+$string['info'] = 'Info';
+// content
+$string['currentorg'] = 'The user\'s current organisation';
+$string['currentpos'] = 'The user\'s current position';
+$string['user'] = 'The user';
+$string['date'] = 'The date';
