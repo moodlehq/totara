ab786dd96eed74ac6af719b55a6d4f656c8506bc
Chris Wharton <chrisw@catalyst.net.nz>
2011-01-12 16:00:46 +1300

    T-6862 mod/feedback/rb_sources/rb_source_graphical_feedback_questions.php: added lang string references lang/en_utf8/rb_source_graphical_feedback_questions.php: created lang file

---
+++ lang/en_utf8/rb_source_graphical_feedback_questions.php
@@ -0,0 +1,37 @@
+<?php
+// lang file for mod/feedback/rb_sources/rb_source_graphical_feedback_questions.php
+// columns
+$string['timecompleted'] = 'Time Completed';
+$string['feedbackactivity'] = 'Feedback Activity';
+$string['ftfsessionid'] = 'Face to face Session ID';
+$string['trainerid'] = 'Trainer ID';
+$string['trainerfullname'] = 'Trainer Full Name';
+$string['trainerorgid'] = 'Trainer Organisation ID';
+$string['trainerorg'] = 'Trainer Organisation';
+$string['trainerposid'] = 'Trainer Position ID';
+$string['trainerpos'] = 'Trainer Position';
+$string['numfeedbackresponses'] = '# Feedback Responses';
+$string['percentoption'] = ': % option';
+$string['numoption'] = ': # option';
+$string['numresponses'] = ': # Responses';
+$string['average'] = ' Average';
+$string['numanswers'] = ': # answers';
+$string['allanswers'] = ': All answers';
+$string['sum'] = ': Sum';
+$string['min'] = ': Min';
+$string['max'] = ': Max';
+$string['stddev'] = ': Standard Deviation';
+
+// filters
+$string['feedbackname'] = 'Feedback Name';
+$string['numofresponses'] = 'Number of responses';
+$string['timecompleted'] = 'Time completed';
+
+
+// content
+$string['theuser'] = 'The user';
+$string['thetrainer'] = 'The trainer';
+$string['currentpos'] = 'The user\'s current position';
+$string['currentorg'] = 'The user\'s current organisation';
+$string['course'] = 'The course';
+$string['responsetime'] = 'The response time';
--- mod/feedback/rb_sources/rb_source_graphical_feedback_questions.php
+++ mod/feedback/rb_sources/rb_source_graphical_feedback_questions.php
@@ -1,7 +1,7 @@
 <?php // $Id$
 
 /*
- * mod/feedback/rb_sources/rb_source_feedback_questions.php
+ * mod/feedback/rb_sources/rb_source_graphical_feedback_questions.php
  *
  * Report Builder source for generating question-level reports on feedback
  * activities. Requires the rb_preproc_feedback_questions preprocessor and
@@ -133,62 +133,62 @@ class rb_source_graphical_feedback_questions extends rb_base_source {
             new rb_column_option(
                 'responses',
                 'timecompleted',
-                'Time Completed',
+                get_string('timecompleted', 'rb_source_graphical_feedback_questions'),
                 'base.completedtime',
                 array('displayfunc' => 'nice_datetime')
             ),
             new rb_column_option(
                 'feedback',
                 'name',
-                'Feedback Activity',
+                get_string('feedbackactivity', 'rb_source_graphical_feedback_questions'),
                 'feedback.name',
                 array('joins' => 'feedback')
             ),
             new rb_column_option(
                 'trainer',
                 'id',
-                'Face-to-face Session ID',
+                get_string('ftfsessionid', 'rb_source_graphical_feedback_questions'),
                 'base.sessionid'
             ),
             new rb_column_option(
                 'trainer',
                 'id',
-                'Trainer ID',
+                get_string('trainerid', 'rb_source_graphical_feedback_questions'),
                 'sessiontrainer.userid',
                 array('joins' => 'sessiontrainer')
             ),
             new rb_column_option(
                 'trainer',
                 'fullname',
-                'Trainer Fullname',
+                get_string('trainerfullname', 'rb_source_graphical_feedback_questions'),
                 sql_fullname('trainer.firstname', 'trainer.lastname'),
                 array('joins' => 'trainer')
             ),
             new rb_column_option(
                 'trainer',
                 'organisationid',
-                'Trainer Organisation ID',
+                get_string('trainerorgid', 'rb_source_graphical_feedback_questions'),
                 'trainer_pa.organisationid',
                 array('joins' => 'trainer_position_assignment')
             ),
             new rb_column_option(
                 'trainer',
                 'organisation',
-                'Trainer Organisation',
+                get_string('trainerorg', 'rb_source_graphical_feedback_questions'),
                 'trainer_organisation.fullname',
                 array('joins' => 'trainer_organisation')
             ),
             new rb_column_option(
                 'trainer',
                 'positionid',
-                'Trainer Position ID',
+                get_string('trainerposid', 'rb_source_graphical_feedback_questions'),
                 'trainer_pa.positionid',
                 array('joins' => 'trainer_position_assignment')
             ),
             new rb_column_option(
                 'trainer',
                 'position',
-                'Trainer Position',
+                get_string('trainerpos', 'rb_source_graphical_feedback_questions'),
                 'trainer_position.fullname',
                 array('joins' => 'trainer_position')
             ),
@@ -212,7 +212,7 @@ class rb_source_graphical_feedback_questions extends rb_base_source {
             new rb_column(
                 'responses',
                 'number',
-                '# Feedback Responses',
+                get_string('numfeedbackresponses', 'rb_source_graphical_feedback_questions'),
                 'base.id',
                 array('grouping' => 'count')
             ),
@@ -240,7 +240,7 @@ class rb_source_graphical_feedback_questions extends rb_base_source {
                                 $requiredcolumns[] = new rb_column(
                                     'q' . $qid,
                                     $oid . '_sum',
-                                    'Q' . $qid . ': # option ' . $oid,
+                                    'Q' . $qid . get_string('numoption', 'rb_source_graphical_feedback_questions'), . $oid,
                                     'base.q' . $qid . '_' . $oid,
                                     array('grouping' => 'sum')
                                 );
@@ -248,7 +248,7 @@ class rb_source_graphical_feedback_questions extends rb_base_source {
                                 $requiredcolumns[] = new rb_column(
                                     'q' . $qid,
                                     $oid . '_perc',
-                                    'Q' . $qid . ': % option ' . $oid,
+                                    'Q' . $qid . get_string('percentoption', 'rb_source_graphical_feedback_questions'), . $oid,
                                     'base.q' . $qid . '_' . $oid,
                                     array('grouping' => 'percent')
                                 );
@@ -257,7 +257,7 @@ class rb_source_graphical_feedback_questions extends rb_base_source {
                             $requiredcolumns[] = new rb_column(
                                 'q' . $qid,
                                 'total',
-                                'Q' . $qid . ' # Responses',
+                                'Q' . $qid . get_string('numresponses', 'rb_source_graphical_feedback_questions'),
                                 'base.q' . $qid . '_value',
                                 array('grouping' => 'count')
                             );
@@ -265,7 +265,7 @@ class rb_source_graphical_feedback_questions extends rb_base_source {
                             $requiredcolumns[] = new rb_column(
                                 'q' . $qid,
                                 'average',
-                                'Q' . $qid . ' Average',
+                                'Q' . $qid . get_string('average', 'rb_source_graphical_feedback_questions'),
                                 'base.q' . $qid . '_value',
                                 array(
                                     'displayfunc' => 'round2',
@@ -280,7 +280,7 @@ class rb_source_graphical_feedback_questions extends rb_base_source {
                         $requiredcolumns[] = new rb_column(
                             'q' . $qid,
                             'count',
-                            'Q' . $qid . ': # answers',
+                            'Q' . $qid . get_string('numanswers', 'rb_source_graphical_feedback_questions'),
                             'base.q' . $qid . '_answer',
                             array('grouping' => 'count')
                         );
@@ -288,7 +288,7 @@ class rb_source_graphical_feedback_questions extends rb_base_source {
                         $requiredcolumns[] = new rb_column(
                             'q' . $qid,
                             'list',
-                            'Q' . $qid . ': All answers',
+                            'Q' . $qid . get_string('allanswers', 'rb_source_graphical_feedback_questions'),
                             'base.q' . $qid . '_answer',
                             array(
                                 'grouping' => 'list_dash',
@@ -302,7 +302,7 @@ class rb_source_graphical_feedback_questions extends rb_base_source {
                         $requiredcolumns[] = new rb_column(
                             'q' . $qid,
                             'count',
-                            'Q' . $qid . ': # answers',
+                            'Q' . $qid . get_string('numanswers', 'rb_source_graphical_feedback_questions'),
                             'base.q' . $qid . '_answer',
                             array('grouping' => 'count')
                         );
@@ -310,7 +310,7 @@ class rb_source_graphical_feedback_questions extends rb_base_source {
                         $requiredcolumns[] = new rb_column(
                             'q' . $qid,
                             'list',
-                            'Q' . $qid . ': All answers',
+                            'Q' . $qid . get_string('allanswers', 'rb_source_graphical_feedback_questions'),
                             'base.q' . $qid . '_answer',
                             array(
                                 'grouping' => 'list_dash',
@@ -321,7 +321,7 @@ class rb_source_graphical_feedback_questions extends rb_base_source {
                         $requiredcolumns[] = new rb_column(
                             'q' . $qid,
                             'sum',
-                            'Q' . $qid . ': Sum',
+                            'Q' . $qid . get_string('sum', 'rb_source_graphical_feedback_questions'),
                             'base.q' . $qid . '_answer',
                             array('grouping' => 'sum')
                         );
@@ -340,7 +340,7 @@ class rb_source_graphical_feedback_questions extends rb_base_source {
                         $requiredcolumns[] = new rb_column(
                             'q' . $qid,
                             'min',
-                            'Q' . $qid . ': Min',
+                            'Q' . $qid . get_string('min', 'rb_source_graphical_feedback_questions'),
                             'base.q' . $qid . '_answer',
                             array('grouping' => 'min')
                         );
@@ -348,7 +348,7 @@ class rb_source_graphical_feedback_questions extends rb_base_source {
                         $requiredcolumns[] = new rb_column(
                             'q' . $qid,
                             'max',
-                            'Q' . $qid . ': Max',
+                            'Q' . $qid . get_string('max', 'rb_source_graphical_feedback_questions'),
                             'base.q' . $qid . '_answer',
                             array('grouping' => 'max')
                         );
@@ -356,7 +356,7 @@ class rb_source_graphical_feedback_questions extends rb_base_source {
                         $requiredcolumns[] = new rb_column(
                             'q' . $qid,
                             'stddev',
-                            'Q' . $qid . ': Standard Deviation',
+                            'Q' . $qid . get_string('stddev', 'rb_source_graphical_feedback_questions'),
                             'base.q' . $qid . '_answer',
                             array(
                                 'displayfunc' => 'round2',
@@ -379,31 +379,31 @@ class rb_source_graphical_feedback_questions extends rb_base_source {
             new rb_filter_option(
                 'feedback',
                 'name',
-                'Feedback Name',
+                get_string('feedbackname', 'rb_source_graphical_feedback_questions'),
                 'text'
             ),
             new rb_filter_option(
                 'responses',
                 'number',
-                'Number of responses',
+                get_string('numofresponses', 'rb_source_graphical_feedback_questions'),
                 'number'
             ),
             new rb_filter_option(
                 'responses',
                 'timecompleted',
-                'Time completed',
+                get_string('timecompleted', 'rb_source_graphical_feedback_questions'),
                 'date'
             ),
             new rb_filter_option(
                 'trainer',
                 'fullname',
-                'Trainer Fullname',
+                get_string('trainerfullname', 'rb_source_graphical_feedback_questions'),
                 'text'
             ),
             new rb_filter_option(
                 'trainer',
                 'organisationid',
-                'Trainer Organisation',
+                get_string('trainerorg', 'rb_source_graphical_feedback_questions'),
                 'select',
                 array(
                     'selectfunc' => 'organisations_list',
@@ -413,7 +413,7 @@ class rb_source_graphical_feedback_questions extends rb_base_source {
             new rb_filter_option(
                 'trainer',
                 'positionid',
-                'Trainer Position',
+                get_string('trainerpos', 'rb_source_graphical_feedback_questions'),
                 'select',
                 array(
                     'selectfunc' => 'positions_list',
@@ -439,34 +439,34 @@ class rb_source_graphical_feedback_questions extends rb_base_source {
         $contentoptions = array(
             new rb_content_option(
                 'user',
-                'The user',
+                get_string('theuser', 'rb_source_graphical_feedback_questions'),
                 'base.userid'
             ),
             new rb_content_option(
                 'current_pos',
-                "The user's current position",
+                get_string('currentpos', 'rb_source_graphical_feedback_questions'),
                 'base.userid'
             ),
             new rb_content_option(
                 'current_org',                      // class name
-                "The user's current organisation",  // title
+                get_string('currentorg', 'rb_source_graphical_feedback_questions'),  // title
                 'base.userid'                      // field
             ),
             new rb_content_option(
                 'course_tag',
-                'The course',
+                get_string('course', 'rb_source_graphical_feedback_questions'),
                 'tagids.idlist',
                 'tagids'
             ),
             new rb_content_option(
                 'trainer',
-                'The trainer',
+                get_string('thetrainer', 'rb_source_graphical_feedback_questions'),
                 'sessiontrainer.userid',
                 'sessiontrainer'
             ),
             new rb_content_option(
                 'date',
-                "The response time",
+                get_string('responsetime', 'rb_source_graphical_feedback_questions'),
                 'base.completedtime'
             ),
         );
@@ -559,6 +559,6 @@ class rb_source_graphical_feedback_questions extends rb_base_source {
     //
 
 
-} // end of rb_source_feedback_questions class
+} // end of rb_source_graphical_feedback_questions class
 
 
