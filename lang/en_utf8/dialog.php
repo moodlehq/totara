7467d0587640f76743b03723b2daac90f176f19c
Jake Salmon <jake.salmon@kineo.com>
2011-01-14 11:55:50 +0000

    T-6976 theme/totara: When javascript is enabled, the user now gets a dialog notification (to replace the normal green/red notifications at the top of the page). The jQuery UI files are now included on every page via meta.php. Some work needs to be done to remove the ability for pages to manually call in the jQuery ui files are they are now always present.

--- lang/en_utf8/dialog.php
+++ lang/en_utf8/dialog.php
@@ -4,3 +4,6 @@ $string['browse'] = 'Browse';
 $string['currentlyselected'] = 'Currently selected';
 $string['error:dialognotreeitems'] = 'No items available';
 $string['search'] = 'Search';
+$string['actionnotification'] = 'Action notification';
+$string['changessaved'] = 'The changes made have been saved, click the “Continue” button to return';
+$string['changesnotsave'] = 'The changes have not been saved, click the "Continue" button to return';
--- theme/totara/meta.php
+++ theme/totara/meta.php
@@ -5,12 +5,16 @@
     <link rel="stylesheet" type="text/css" href="<?php echo $CFG->httpsthemewww ?>/totara/styles_ie7.css" />
 <![endif]-->
 
+<link rel="stylesheet" type="text/css" href="<?php echo $CFG->wwwroot ?>/local/js/lib/ui-lightness/jquery-ui-1.7.2.custom.css" />
+
 <?php
 
     require_js(
         array(
             $CFG->wwwroot.'/local/js/lib/jquery-1.3.2.min.js',
+	    $CFG->wwwroot.'/local/js/lib/jquery-ui-1.7.2.custom.min.js',
             $CFG->wwwroot.'/local/js/global.js',
+	    $CFG->wwwroot.'/local/js/dialog.notifications.js.php'
         )
     );
 
