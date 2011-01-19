0001f1d6c4aa27320f95889effb2057e32178071
Jake Salmon <jake.salmon@kineo.com>
2011-01-19 15:04:43 +0000

    T-7379 hierarchy/type/organisation/tabs.php: replace the second level of tabs with a dropdown menu

--- hierarchy/type/organisation/tabs.php
+++ hierarchy/type/organisation/tabs.php
@@ -19,7 +19,9 @@ $toprow[] = new tabobject('competencies', $CFG->wwwroot.'/hierarchy/item/view.ph
 
 $assignedcounts = get_records_sql_menu("SELECT comp.frameworkid, COUNT(*) from {$CFG->prefix}org_competencies orgcomp JOIN {$CFG->prefix}comp comp ON orgcomp.competencyid=comp.id where orgcomp.organisationid={$id} GROUP BY comp.frameworkid");
 
+
 if(substr($currenttab, 0, 12) == 'competencies'){
+    /*
     if($frameworks){
         foreach($frameworks as $framework){
             $count = isset($assignedcounts[$framework->id]) ? $assignedcounts[$framework->id] : 0;
@@ -31,8 +33,10 @@ if(substr($currenttab, 0, 12) == 'competencies'){
             $activated[] = 'competencies'.$comptab;
         }
     }
+    */
     $currenttab = 'competencies';
 }
+
 
 /*$toprow[] = new tabobject('comptemplates', $CFG->wwwroot.'/hierarchy/item/view.php?id='.$id.'&edit='.$edit.'&type=organisation&comptype=comptemplates', get_string('competencytemplates', 'competency'));
 if(substr($currenttab, 0, 13) == 'comptemplates'){
@@ -49,10 +53,19 @@ if(substr($currenttab, 0, 13) == 'comptemplates'){
     $currenttab = 'comptemplates';
 }*/
 
-$tabs = array($toprow, $secondrow);
+$tabs = array($toprow);
 
 print_heading(get_string('assignedcompetencies', 'competency'));
 // print out tabs
 print_tabs($tabs, $currenttab, $inactive, $activated);
+
+// print out the competency framework selector
+$fwoptions = array();
+echo '<div class="frameworkpicker">';
+foreach ($frameworks as $fw) {
+    $fwoptions[$fw->id] = $fw->fullname;
+}
+popup_form($CFG->wwwroot.'/hierarchy/item/view.php?id='.$id.'&amp;edit='.$edit.'&amp;type='.$type.'&amp;framework=', $fwoptions, 'switchframework', $fid, '', '', '', false, 'self', get_string('switchframework', 'hierarchy'));
+echo '</div>';
 
 ?>
