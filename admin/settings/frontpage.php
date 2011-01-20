a615adcaaa0449b9b0a83d5b59de9ef2732077be
Chris Wharton <chrisw@catalyst.net.nz>
2011-01-21 10:57:33 +1300

    T-7277 admin/settings/frontpage.php: changed default display of My course completions to false

--- admin/settings/frontpage.php
+++ admin/settings/frontpage.php
@@ -24,7 +24,7 @@ if (get_site()) { //do not use during installation
         $temp->add(new admin_setting_sitesettext('fullname', get_string('fullsitename'), '', NULL)); // no default
         $temp->add(new admin_setting_sitesettext('shortname', get_string('shortsitename'), '', NULL)); // no default
         $temp->add(new admin_setting_special_frontpagedesc());
-        $temp->add(new admin_setting_configcheckbox('courseprogress', get_string('courseprogress','completion'), get_string('courseprogresshelp','completion'), 1));
+        $temp->add(new admin_setting_configcheckbox('courseprogress', get_string('courseprogress','completion'), get_string('courseprogresshelp','completion'), 0));
         $temp->add(new admin_setting_courselist_frontpage(false)); // non-loggedin version of the setting (that's what the parameter is for :) )
         $temp->add(new admin_setting_courselist_frontpage(true)); // loggedin version of the setting
         
