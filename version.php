6ee3e2bdfa1b1fefea4916c7f9a278f1bcb9849e
Aaron Barnes <aaronb@catalyst.net.nz>
2011-01-25 15:13:27 +1300

    T-7189 version.php: Tagging version 1.0

--- version.php
+++ version.php
@@ -19,7 +19,7 @@
 // This can be used for modules to set a minimum functionality requirement.
 
     $TOTARA = new object();
-    $TOTARA->version    = '0.9+';               # Please keep as string
-    $TOTARA->build      = '20101223.00';        # Please keep as string
+    $TOTARA->version    = '1.0';               # Please keep as string
+    $TOTARA->build      = '20110125.00';        # Please keep as string
     $TOTARA->release    = "{$TOTARA->version} (Build: {$TOTARA->build})";
 
