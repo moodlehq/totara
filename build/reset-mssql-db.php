9e7388bc791d214b440a8897f2f289250fa0265c
Aaron Barnes <aaronb@catalyst.net.nz>
2011-02-02 16:38:28 +1300

    T-6080 build: Create MSSQL build script
    Change-Id: I903f3c8d90b8e1d66b32a381a2c43a1d9be7e511
    Reviewed-on: http://review.dev.totaralms.com/11
    Reviewed-by: Aaron Barnes <aaronb@catalyst.net.nz>
    Tested-by: Aaron Barnes <aaronb@catalyst.net.nz>

---
+++ build/reset-mssql-db.php
@@ -0,0 +1,21 @@
+<?php
+
+    error_reporting(E_ALL);
+    ini_set('display_errors', '1');
+
+    print 'Connect to database server';
+
+    $link = mssql_connect('192.168.198.44', 'hudson', 'hudson');
+    if (!$link) {
+        die('Could not connect: ' . mssql_error());
+    }
+
+    print 'Drop database hudson';
+    mssql_query('DROP DATABASE hudson');
+
+    print 'Create database hudson';
+    mssql_query('CREATE DATABASE hudson');
+    mssql_query('ALTER DATABASE hudson SET ANSI_NULLS ON');
+    mssql_query('ALTER DATABASE hudson SET QUOTED_IDENTIFIER ON');
+
+    mssql_close($link);
