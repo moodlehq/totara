a21aad4cf80564ad04aa64f95d614771d982d163
Aaron Barnes <aaronb@catalyst.net.nz>
2011-02-15 13:18:11 +1300

    T-6080 build/simpletests.py: Write xml output directly to disk
    Change-Id: I0be235f9db12726ed47a02220fb15158d2fb477c
    Reviewed-on: http://review.dev.totaralms.com/71
    Reviewed-by: Aaron Barnes <aaronb@catalyst.net.nz>
    Tested-by: Aaron Barnes <aaronb@catalyst.net.nz>

--- build/simpletests.py
+++ build/simpletests.py
@@ -21,10 +21,5 @@ mech["username"] = "admin"
 mech["password"] = "passworD1!"
 mech.submit()
 
-print 'Run unit tests'
-tests = mech.open(rooturl+'admin/report/unittest/xml.php')
-
-print 'Write to file'
-f = open('build/logs/simpletest-results.xml', 'w')
-print >> f, tests.read()
-f.close()
+print 'Run unit tests and write to file'
+mech.retrieve(rooturl+'admin/report/unittest/xml.php', 'build/logs/simpletest-results.xml')
