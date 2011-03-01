5fa92a900437cdec14c9b8d90aa9379ebc829bd5
Alastair Munro <alastair@catalyst.net.nz>
2011-03-01 15:32:41 +1300

    T-7611 theme/: added changed and images for themes from Dave W
    Change-Id: I84a7352d9256736f999cf35e32ca740d071bcc81
    Reviewed-on: http://review.dev.totaralms.com/170
    Reviewed-by: Aaron Barnes <aaronb@catalyst.net.nz>
    Tested-by: Aaron Barnes <aaronb@catalyst.net.nz>

--- theme/sector_hospitality/meta.php
+++ theme/sector_hospitality/meta.php
@@ -1,3 +1,3 @@
 <!--[if IE 6]>
-    <link rel="stylesheet" type="text/css" href="<?php echo $CFG->httpsthemewww ?>/sector_food/styles_ie6.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo $CFG->httpsthemewww ?>/sector_hospitality/styles_ie6.css" />
 <![endif]-->
