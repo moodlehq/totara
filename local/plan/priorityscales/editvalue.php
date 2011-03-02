87a921911b6f867ad2c0b48e1e7a778c56bb3100
Alastair Munro <alastair@catalyst.net.nz>
2011-03-03 09:45:14 +1300

    T-6072 local/plan: fix whitespace
    Change-Id: I259317030c1f494c6629b72067e6a5d8d71b6269
    Reviewed-on: http://review.dev.totaralms.com/182
    Reviewed-by: Totara Hudson <totara-hudson@catalyst.net.nz>
    Reviewed-by: Chris Wharton <chrisw@catalyst.net.nz>
    Reviewed-by: Aaron Barnes <aaronb@catalyst.net.nz>
    Tested-by: Aaron Barnes <aaronb@catalyst.net.nz>

--- local/plan/priorityscales/editvalue.php
+++ local/plan/priorityscales/editvalue.php
@@ -3,13 +3,13 @@
  * This file is part of Totara LMS
  *
  * Copyright (C) 2010, 2011 Totara Learning Solutions LTD
- * Copyright (C) 1999 onwards Martin Dougiamas 
- * 
- * This program is free software; you can redistribute it and/or modify  
- * it under the terms of the GNU General Public License as published by  
- * the Free Software Foundation; either version 2 of the License, or     
- * (at your option) any later version.                                   
- *                                                                       
+ * Copyright (C) 1999 onwards Martin Dougiamas
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -20,7 +20,7 @@
  *
  * @author Alastair Munro <alastair@catalyst.net.nz>
  * @package totara
- * @subpackage plan 
+ * @subpackage plan
  */
 
 require_once('../../../config.php');
