8c4f7e451fb2876f029a205a7fd1ed4486497601
Aaron Barnes <aaronb@catalyst.net.nz>
2011-03-25 13:27:03 +1300

    T-6072 local/: Fix whitespace
    Change-Id: Ie532999b5f3ce6d78128dc5d58d3874a4769b515
    Reviewed-on: http://review.dev.totaralms.com/285
    Reviewed-by: Alastair Munro <alastair@catalyst.net.nz>
    Reviewed-by: Aaron Barnes <aaronb@catalyst.net.nz>
    Tested-by: Aaron Barnes <aaronb@catalyst.net.nz>

--- local/version.php
+++ local/version.php
@@ -3,12 +3,12 @@
  * This file is part of Totara LMS
  *
  * Copyright (C) 2010, 2011 Totara Learning Solutions LTD
- * 
- * This program is free software; you can redistribute it and/or modify  
- * it under the terms of the GNU General Public License as published by  
- * the Free Software Foundation; either version 2 of the License, or     
- * (at your option) any later version.                                   
- *                                                                       
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -17,9 +17,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @author [author name] <[email address]>
+ * @author Aaron Barnes <aaron.barnes@totaralms.com>
  * @package totara
- * @subpackage local 
+ * @subpackage local
  */
 
 /**
