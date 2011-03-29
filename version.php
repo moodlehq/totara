fd04aae030b5b523b2823cb5d7163f5b4d637d42
Aaron Barnes <aaronb@catalyst.net.nz>
2011-03-29 15:48:55 +1300

    T-7189 version.php: Tagging version 1.0.5
    Change-Id: Id632c78596f5a905822e620c20366f80df5e7ca5
    Reviewed-on: http://review.dev.totaralms.com/310
    Tested-by: Totara Hudson <totara-hudson@catalyst.net.nz>
    Reviewed-by: Alastair Munro <alastair@catalyst.net.nz>
    Reviewed-by: Aaron Barnes <aaronb@catalyst.net.nz>

--- version.php
+++ version.php
@@ -19,7 +19,7 @@
 // This can be used for modules to set a minimum functionality requirement.
 
     $TOTARA = new object();
-    $TOTARA->version    = '1.0.4+';             # Please keep as string
-    $TOTARA->build      = '20110322.00';        # Please keep as string
+    $TOTARA->version    = '1.0.5';              # Please keep as string
+    $TOTARA->build      = '20110329.00';        # Please keep as string
     $TOTARA->release    = "{$TOTARA->version} (Build: {$TOTARA->build})";
 
