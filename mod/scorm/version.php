0210749c7fec40922a41dbcbb74428820a6a4a46
Aaron Barnes <aaronb@catalyst.net.nz>
2011-04-26 14:13:32 +1200

    T-7840 scorm: Track SCORM data points in activity completion
    Change-Id: Ie64e25ffa2d4754183e5be65d1a38219615b2daa
    Reviewed-on: http://review.dev.totaralms.com/452
    Tested-by: Totara Hudson <totara-hudson@catalyst.net.nz>
    Reviewed-by: Alastair Munro <alastair@catalyst.net.nz>
    Reviewed-by: Simon Coggins <simonc@catalyst.net.nz>

--- mod/scorm/version.php
+++ mod/scorm/version.php
@@ -6,11 +6,7 @@
 /////////////////////////////////////////////////////////////////////////////////
 
 
-// NOTE  The version below was accidentally set a month into the future!  We need to 
-//       catch up now, so until 27th October please only increment in very tiny steps 
-//       in HEAD, until we get past that date..
-
-$module->version  = 2007110503;   // The (date) version of this module
+$module->version  = 2011041402;   // The (date) version of this module
 $module->requires = 2007101509;   // The version of Moodle that is required
 $module->cron     = 300;            // How often should cron check this module (seconds)?
 
