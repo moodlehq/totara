7c1d5ed89d9208a7e66e871a3c357884ef164192
Aaron Barnes <aaronb@catalyst.net.nz>
2011-04-29 14:12:49 +1200

    T-8069 blocks/completionstatus/details.php: Support courses with no criteria
    Change-Id: Ib6da6c66a517c6929edc20d42fe1e8f8ad67e26c
    Reviewed-on: http://review.dev.totaralms.com/423
    Tested-by: Totara Hudson <totara-hudson@catalyst.net.nz>
    Reviewed-by: Alastair Munro <alastair@catalyst.net.nz>
    Reviewed-by: Simon Coggins <simonc@catalyst.net.nz>

--- blocks/completionstatus/details.php
+++ blocks/completionstatus/details.php
@@ -61,14 +61,6 @@ if (!$info->is_enabled()) {
     print_error('completionnotenabled', 'completion', $returnurl);
 }
 
-// Load criteria to display
-$completions = $info->get_completions($user->id);
-
-// Check if this course has any criteria
-if (empty($completions)) {
-    print_error('err_nocriteria', 'completion', $returnurl);
-}
-
 // Check this user is enroled
 if (!$info->is_tracked_user($user->id)) {
     if ($USER->id == $user->id) {
@@ -141,117 +133,128 @@ if (isset($ccompletion) && strlen($ccompletion->rpl)) {
     echo '<tr><td colspan="2"><b>'.get_string('courserpl', 'completion').':</b> '.format_string($ccompletion->rpl).'</td></tr>';
 }
 
-echo '<tr><td colspan="2"><b>'.get_string('required').':</b> ';
+// Load criteria to display
+$completions = $info->get_completions($user->id);
 
-// Get overall aggregation method
-$overall = $info->get_aggregation_method();
-
-if ($overall == COMPLETION_AGGREGATION_ALL) {
-    echo get_string('criteriarequiredall', 'completion');
+// Check if this course has any criteria
+if (empty($completions)) {
+    echo '<tr><td colspan="2"><br />';
+    echo print_box(get_string('err_nocriteria', 'completion'), 'noticebox');
+    echo '</td></tr></tbody></table>';
 } else {
-    echo get_string('criteriarequiredany', 'completion');
-}
+    echo '<tr><td colspan="2"><b>'.get_string('required').':</b> ';
 
-echo '</td></tr></tbody></table>';
+    // Get overall aggregation method
+    $overall = $info->get_aggregation_method();
 
-// Generate markup for criteria statuses
-echo '<table class="generalbox logtable boxaligncenter" id="criteriastatus" width="100%"><tbody>';
-echo '<tr class="ccheader">';
-echo '<th class="c0 header" scope="col">'.get_string('criteriagroup', 'block_completionstatus').'</th>';
-echo '<th class="c1 header" scope="col">'.get_string('criteria', 'completion').'</th>';
-echo '<th class="c2 header" scope="col">'.get_string('requirement', 'block_completionstatus').'</th>';
-echo '<th class="c3 header" scope="col">'.get_string('status').'</th>';
-echo '<th class="c4 header" scope="col">'.get_string('complete').'</th>';
-echo '<th class="c5 header" scope="col">'.get_string('completiondate', 'coursereport_completion').'</th>';
-echo '</tr>';
-
-// Save row data
-$rows = array();
-
-global $COMPLETION_CRITERIA_TYPES;
-
-// Loop through course criteria
-foreach ($completions as $completion) {
-    $criteria = $completion->get_criteria();
-
-    $row = array();
-    $row['type'] = $criteria->criteriatype;
-    $row['title'] = $criteria->get_title();
-    $row['status'] = $completion->is_complete();
-    $row['timecompleted'] = $completion->timecompleted;
-    $row['details'] = $criteria->get_details($completion);
-    $rows[] = $row;
-}
-
-// Print table
-$last_type = '';
-$agg_type = false;
-$oddeven = 0;
-
-foreach ($rows as $row) {
-
-    echo '<tr class="r' . $oddeven . '">';
-
-    // Criteria group
-    echo '<td class="cell c0">';
-    if ($last_type !== $row['details']['type']) {
-        $last_type = $row['details']['type'];
-        echo $last_type;
-
-        // Reset agg type
-        $agg_type = true;
+    if ($overall == COMPLETION_AGGREGATION_ALL) {
+        echo get_string('criteriarequiredall', 'completion');
     } else {
-        // Display aggregation type
-        if ($agg_type) {
-            $agg = $info->get_aggregation_method($row['type']);
-
-            echo '(<i>';
-
-            if ($agg == COMPLETION_AGGREGATION_ALL) {
-                echo strtolower(get_string('all', 'completion'));
-            } else {
-                echo strtolower(get_string('any', 'completion'));
-            }
-
-            echo '</i> '.strtolower(get_string('required')).')';
-            $agg_type = false;
-        }
+        echo get_string('criteriarequiredany', 'completion');
     }
-    echo '</td>';
 
-    // Criteria title
-    echo '<td class="cell c1">';
-    echo $row['details']['criteria'];
-    echo '</td>';
+    echo '</td></tr></tbody></table>';
 
-    // Requirement
-    echo '<td class="cell c2">';
-    echo $row['details']['requirement'];
-    echo '</td>';
-
-    // Status
-    echo '<td class="cell c3">';
-    echo $row['details']['status'];
-    echo '</td>';
-
-    // Is complete
-    echo '<td class="cell c4">';
-    echo $row['status'] ? get_string('yes') : get_string('no');
-    echo '</td>';
-
-    // Completion data
-    echo '<td class="cell c5">';
-    if ($row['timecompleted']) {
-        echo userdate($row['timecompleted'], '%e %B %G');
-    } else {
-        echo '-';
-    }
-    echo '</td>';
+    // Generate markup for criteria statuses
+    echo '<table class="generalbox logtable boxaligncenter" id="criteriastatus" width="100%"><tbody>';
+    echo '<tr class="ccheader">';
+    echo '<th class="c0 header" scope="col">'.get_string('criteriagroup', 'block_completionstatus').'</th>';
+    echo '<th class="c1 header" scope="col">'.get_string('criteria', 'completion').'</th>';
+    echo '<th class="c2 header" scope="col">'.get_string('requirement', 'block_completionstatus').'</th>';
+    echo '<th class="c3 header" scope="col">'.get_string('status').'</th>';
+    echo '<th class="c4 header" scope="col">'.get_string('complete').'</th>';
+    echo '<th class="c5 header" scope="col">'.get_string('completiondate', 'coursereport_completion').'</th>';
     echo '</tr>';
-    // for row striping
-    $oddeven = $oddeven ? 0 : 1;
-}
 
-echo '</tbody></table>';
+    // Save row data
+    $rows = array();
+
+    global $COMPLETION_CRITERIA_TYPES;
+
+    // Loop through course criteria
+    foreach ($completions as $completion) {
+        $criteria = $completion->get_criteria();
+        $complete = $completion->is_complete();
+
+        $row = array();
+        $row['type'] = $criteria->criteriatype;
+        $row['title'] = $criteria->get_title();
+        $row['status'] = $completion->get_status();
+        $row['timecompleted'] = $completion->timecompleted;
+        $row['details'] = $criteria->get_details($completion);
+        $rows[] = $row;
+    }
+
+    // Print table
+    $last_type = '';
+    $agg_type = false;
+    $oddeven = 0;
+
+    foreach ($rows as $row) {
+
+        echo '<tr class="r' . $oddeven . '">';
+
+        // Criteria group
+        echo '<td class="cell c0">';
+        if ($last_type !== $row['details']['type']) {
+            $last_type = $row['details']['type'];
+            echo $last_type;
+
+            // Reset agg type
+            $agg_type = true;
+        } else {
+            // Display aggregation type
+            if ($agg_type) {
+                $agg = $info->get_aggregation_method($row['type']);
+
+                echo '(<i>';
+
+                if ($agg == COMPLETION_AGGREGATION_ALL) {
+                    echo strtolower(get_string('all', 'completion'));
+                } else {
+                    echo strtolower(get_string('any', 'completion'));
+                }
+
+                echo '</i> '.strtolower(get_string('required')).')';
+                $agg_type = false;
+            }
+        }
+        echo '</td>';
+
+        // Criteria title
+        echo '<td class="cell c1">';
+        echo $row['details']['criteria'];
+        echo '</td>';
+
+        // Requirement
+        echo '<td class="cell c2">';
+        echo $row['details']['requirement'];
+        echo '</td>';
+
+        // Status
+        echo '<td class="cell c3">';
+        echo $row['details']['status'];
+        echo '</td>';
+
+        // Is complete
+        echo '<td class="cell c4">';
+        echo ($row['status'] === 'Yes') ? 'Yes' : 'No';
+        echo '</td>';
+
+        // Completion data
+        echo '<td class="cell c5">';
+        if ($row['timecompleted']) {
+            echo userdate($row['timecompleted'], '%e %B %G');
+        } else {
+            echo '-';
+        }
+        echo '</td>';
+        echo '</tr>';
+        // for row striping
+        $oddeven = $oddeven ? 0 : 1;
+    }
+
+    echo '</tbody></table>';
+}
 
 print_footer($course);
