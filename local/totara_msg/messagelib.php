19714ff20a5f2f60cf6504a6aaf584b55c3188fc
Simon Coggins <simonc@catalyst.net.nz>
2011-05-03 07:42:33 +1200

    T-8191 local/totara_msg: Fix hardcoded mdl_ reference
    Change-Id: Ia88edc16885a03f5fc52bf859e3f36349bd849e5
    Reviewed-on: http://review.dev.totaralms.com/430
    Tested-by: Totara Hudson <totara-hudson@catalyst.net.nz>
    Reviewed-by: Alastair Munro <alastair@catalyst.net.nz>

--- local/totara_msg/messagelib.php
+++ local/totara_msg/messagelib.php
@@ -745,7 +745,7 @@ function tm_messages_get($type, $order_by=false, $userto=false, $limit=true, $ro
  * @return int count of messages
  */
 function tm_messages_count($type, $userto=false, $roleid=false) {
-        global $USER;
+        global $USER, $CFG;
 
         // select only particular type
         $processor = get_record('message_processors20', 'name', $type);
@@ -769,8 +769,10 @@ function tm_messages_count($type, $userto=false, $roleid=false) {
 
         // hunt for messages
         $msgs = get_records_sql("SELECT count(m.id) AS count
-                                        FROM (mdl_message20 m INNER JOIN  mdl_message_working20 w ON m.id = w.unreadmessageid) LEFT JOIN mdl_message_metadata d ON (d.messageid = m.id)
-                                        WHERE m.useridto = ".$userid .' AND w.processorid = '.$processor->id.$role_filter);
+            FROM ({$CFG->prefix}message20 m
+            INNER JOIN {$CFG->prefix}message_working20 w ON m.id = w.unreadmessageid)
+            LEFT JOIN {$CFG->prefix}message_metadata d ON (d.messageid = m.id)
+            WHERE m.useridto = ".$userid .' AND w.processorid = '.$processor->id.$role_filter);
         if ($msgs) {
             $msg = array_pop($msgs);
             return $msg->count;
