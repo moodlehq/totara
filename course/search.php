aada9b08cfcbc2db8a6123ce4d47908dd5f4946c
Alastair Munro <alastair.munro@totaralms.com>
2011-08-09 10:44:27 +1200

    T-9112 course/search.php: Fix reference old icon code
    Change-Id: Ibdae48c90475c3899b9bb121c567710ca8a47926
    Reviewed-on: https://review.totaralms.com/743
    Tested-by: Totara Hudson <totara-hudson@catalyst.net.nz>
    Reviewed-by: Alastair Munro <alastair.munro@totaralms.com>
    Reviewed-by: Aaron Barnes <aaron.barnes@totaralms.com>

--- course/search.php
+++ course/search.php
@@ -5,6 +5,7 @@ require_once(dirname(dirname(__FILE__))."/config.php");
 require_once("{$CFG->dirroot}/course/lib.php");
 require_once("{$CFG->dirroot}/local/program/lib.php");
 require_once($CFG->dirroot.'/local/icon/course_icon.class.php');
+require_once($CFG->dirroot.'/local/icon/coursecategory_icon.class.php');
 require_once($CFG->dirroot.'/local/icon/program_icon.class.php');
 
 
@@ -872,6 +873,8 @@ if ($results['categories']) {
         $str->roles    = get_string('assignroles', 'role');
         $str->spacer = '<img src="'.$CFG->wwwroot.'/pix/spacer.gif" class="iconsmall" alt="" /> ';
 
+        $category_icon = new coursecategory_icon();
+
         foreach ($results['categories'] as $cat) {
             ++$i;
             $rowclass = $i % 2 ? 'r0' : 'r1';
@@ -882,7 +885,7 @@ if ($results['categories']) {
 
             echo "<tr class=\"{$rowclass}\">\n";
             echo "<td class=\"cell c0\">";
-            echo local_coursecategory_icon_tag($cat, 'small');
+            echo $category_icon->display($cat, 'small');
             echo "<a $linkcss href=\"{$CFG->wwwroot}/course/index.php?highlightid={$cat->id}#category{$cat->id}\">"
                 . highlight($search, format_string($cat->name)) . "</a></td>\n";
             echo "<td class=\"cell c1\">";
