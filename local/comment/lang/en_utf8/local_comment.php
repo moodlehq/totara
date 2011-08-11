b8fb2a501a0eabdd9eb2bd91c7c5d35f93de0660
Aaron Barnes <aaron.barnes@totaralms.com>
2011-08-11 14:22:04 +1200

    T-6710: Added missing lang strings
    Change-Id: I9feb41c7ce444717328b9c8f112edf3e8430f309
    Reviewed-on: https://review.totaralms.com/771
    Tested-by: Totara Hudson <totara-hudson@catalyst.net.nz>
    Reviewed-by: Aaron Barnes <aaron.barnes@totaralms.com>
    Reviewed-by: Alastair Munro <alastair.munro@totaralms.com>

--- local/comment/lang/en_utf8/local_comment.php
+++ local/comment/lang/en_utf8/local_comment.php
@@ -3,6 +3,7 @@ $string['addcomment'] = 'Add a comment...';
 $string['areyousure'] = 'Are you sure?';
 $string['comment:delete'] = 'Delete comments';
 $string['comment:post'] = 'Post a comment';
+$string['comment:view'] = 'View a comment';
 $string['comments'] = 'Comments';
 $string['commentsrequirelogin'] = 'You need to login to view the comments';
 $string['deletecomment'] = 'Delete this comment';
