27e29d09029e3c5f717febe3773d3fd86f7979c6
Alastair Munro <alastair.munro@totaralms.com>
2011-10-18 16:33:51 +1300

    T-9408 hierarchy: Add RTL support for new hierarchies
    Change-Id: I6d0c591cad8cbeefc556798450eed565cb5d0faf
    Reviewed-on: https://review.totaralms.com/1161
    Reviewed-by: Alastair Munro <alastair.munro@totaralms.com>
    Reviewed-by: Simon Coggins <simon.coggins@totaralms.com>
    Tested-by: Jenkins User <jenkins@totaralms.com>

--- hierarchy/prefix/position/lib.php
+++ hierarchy/prefix/position/lib.php
@@ -358,7 +358,7 @@ class position extends hierarchy {
                 $fwoptions[$fw->id] = $fw->fullname . " ({$count})";
             }
             $fwoptions = count($fwoptions) > 1 ? array(0 => get_string('all')) + $fwoptions : $fwoptions;
-            echo '<div style="text-align: right">';
+            echo '<div class="hierarchyframeworkpicker">';
             popup_form($CFG->wwwroot.'/hierarchy/item/view.php?id='.$positionid.'&amp;edit='.$edit.'&amp;prefix=position&amp;framework=', $fwoptions, 'switchframework', $currentfw, '', '', '', false, 'self', get_string('filterframework', 'hierarchy'));
             echo '</div>';
         } else {
