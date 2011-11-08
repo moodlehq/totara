30e96b4525f742873ae8d87a05201a4bfcde9d96
Alastair Munro <alastair.munro@totaralms.com>
2011-11-09 10:30:52 +1300

    T-9448 lib/dmllib.php: Fix unicode issue in ADODB driver
    Change-Id: I7c075ccf1deb0da987111c3a13c059dd98193041
    Reviewed-on: https://review.totaralms.com/1219
    Reviewed-by: Alastair Munro <alastair.munro@totaralms.com>
    Tested-by: Jenkins User <jenkins@totaralms.com>
    Reviewed-by: Simon Coggins <simon.coggins@totaralms.com>
    Reviewed-by: Ciaran Irvine <ciaran.irvine@totaralms.com>

--- lib/dmllib.php
+++ lib/dmllib.php
@@ -2023,12 +2023,15 @@ function sql_compare_text($fieldname, $numchars=32) {
  * @return string the piece of SQL code to be used in your statement.
  */
 function sql_order_by_text($fieldname, $numchars=32) {
-
     global $CFG;
 
     switch ($CFG->dbfamily) {
         case 'mssql':
-            return 'CONVERT(varchar, ' . $fieldname . ', ' . $numchars . ')';
+            if ($CFG->dbtype == 'mssql_n') {
+                return 'CONVERT(nvarchar, ' . $fieldname . ', ' . $numchars . ')';
+            } else {
+                return 'CONVERT(varchar, ' . $fieldname . ', ' . $numchars . ')';
+            }
             break;
         case 'oracle':
             return 'dbms_lob.substr(' . $fieldname . ', ' . $numchars . ',1)';
