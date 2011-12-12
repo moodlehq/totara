b4ec3f480694e05ffabb92980cf50667510a412b
Alastair Munro <alastair.munro@totaralms.com>
2011-12-13 10:14:37 +1300

    T-9480 mod/certificate/type/: Update unicode certificate types to use new TCPDF library
    Change-Id: I1e3da565cbc2b4bc7116e840f376f274f10aa5b1
    Reviewed-on: https://review.totaralms.com/1294
    Tested-by: Jenkins User <jenkins@totaralms.com>
    Reviewed-by: Simon Coggins <simon.coggins@totaralms.com>
    Reviewed-by: Ciaran Irvine <ciaran.irvine@totaralms.com>

--- mod/certificate/type/unicode_landscape/certificate.php
+++ mod/certificate/type/unicode_landscape/certificate.php
@@ -1,5 +1,4 @@
 <?php
-include '../../lib/tcpdf/tcpdfprotection.php';
 require_once('../../lib/tcpdf/config/lang/eng.php');
 include '../../lib/tcpdf/tcpdf.php';
 
@@ -102,12 +101,12 @@ if (!empty($certificate->coursename)) {
     $classname = $certificate->coursename;
 }
 
-//Create new PDF document 
+//Create new PDF document
 
-    $pdf = new TCPDF_Protection('L', 'pt', 'A4', true); 
+    $pdf = new TCPDF('L', 'pt', 'A4', true);
     $pdf->SetProtection(array('print'));
-    $pdf->print_header = false;
-    $pdf->print_footer = false;
+    $pdf->setPrintHeader = false;
+    $pdf->setPrintFooter = false;
     $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
     $pdf->setLanguageArray($l); //set language items
     $pdf->AddPage();
