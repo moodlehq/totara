5c02b7a2897351cb5ef411201405e87a72f8e5f3
Alastair Munro <alastair.munro@totaralms.com>
2011-12-23 10:48:38 +1300

    T-8485 lang: Changes to language packs
    Language Pack 'fi_utf8' modified
    Language Pack 'nl_utf8' modified
    
    Change-Id: I42c0c3936eb05dd4e9ab9f497ca78f2e12075f3c
    Reviewed-on: https://review.totaralms.com/1339
    Tested-by: Jenkins User <jenkins@totaralms.com>
    Reviewed-by: Simon Coggins <simon.coggins@totaralms.com>
    Reviewed-by: Alastair Munro <alastair.munro@totaralms.com>

--- local/reportbuilder/lang/nl_utf8/local_reportbuilder.php
+++ local/reportbuilder/lang/nl_utf8/local_reportbuilder.php
@@ -1,6 +1,5 @@
-<?PHP // $Id$ 
-      // local_reportbuilder.php - created with Moodle 1.9.14 (Build: 20111010) (2007101591.06)
-
+<?php
+// local_reportbuilder.php - created with Totara langimport script version 1.1
 
 $string['activitygroups'] = 'Activiteitgroepen';
 $string['globalsettings'] = 'Configuratie';
