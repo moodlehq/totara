5f9da5d93eb1acc54a8f973d07c2d17984d34345
Alastair Munro <alastair.munro@totaralms.com>
2012-01-11 09:54:19 +1300

    T-9533 local/plan/index.php: Fix spacing after sentences
    Change-Id: I8fc1f16db5b0751b24615f0cf4f52409edf81830
    Reviewed-on: https://review.totaralms.com/1359
    Reviewed-by: Ciaran Irvine <ciaran.irvine@totaralms.com>
    Tested-by: Jenkins User <jenkins@totaralms.com>
    Reviewed-by: Simon Coggins <simon.coggins@totaralms.com>

--- local/plan/index.php
+++ local/plan/index.php
@@ -77,16 +77,16 @@ if($planuser != $USER->id) {
 print_heading($heading);
 
 print_container_start(false, '', 'dp-plans-description');
-if($planuser == $USER->id) {
-    $planinstructions = '<div class="instructional_text">' . get_string('planinstructions', 'local_plan');
+if ($planuser == $USER->id) {
+    $planinstructions = '<div class="instructional_text">' . get_string('planinstructions', 'local_plan') . ' ';
     add_to_log(SITEID, 'plan', 'view all', "index.php?userid={$planuser}");
 } else {
     $user = get_record('user', 'id', $planuser);
     $userfullname = fullname($user);
-    $planinstructions = '<div class="instructional_text">' . get_string('planinstructionsuser', 'local_plan', $userfullname);
+    $planinstructions = '<div class="instructional_text">' . get_string('planinstructionsuser', 'local_plan', $userfullname) . ' ';
     add_to_log(SITEID, 'plan', 'view all', "index.php?userid={$planuser}", $userfullname);
 }
-if($canaddplan) {
+if ($canaddplan) {
     $planinstructions .= get_string('planinstructions_add', 'local_plan');
 }
 $planinstructions .= '</div>';
