90bb70c7b229a988b83eb73ab1980eb05a076c8f
Simon Coggins <simon.coggins@totaralms.com>
2012-01-20 15:54:31 +1300

    Revert "course/modedit.php: Fix activity completion backport issue"
    This reverts commit ffa574f49a0819a61bb2c4f4b8ff0c110a6e82fd.

--- course/modedit.php
+++ course/modedit.php
@@ -288,13 +288,11 @@
                 $completion=new completion_info($course);
                 $completion->reset_all_state($cm);
             }
-
-            if (!empty($CFG->enablecompletion)) {
-                $cm->completion = $fromform->completion;
-                $cm->completionview = $fromform->completionview;
-                $cm->completionexpected = $fromform->completionexpected;
-                $cm->completiongradeitemnumber = $fromform->completiongradeitemnumber;
-            }
+            set_coursemodule_completion($fromform->coursemodule, $fromform->completion);
+            set_coursemodule_completionview($fromform->coursemodule, $fromform->completionview);
+            set_coursemodule_completionexpected($fromform->coursemodule, $fromform->completionexpected);
+            set_coursemodule_completiongradeitemnumber(
+                    $fromform->coursemodule,$fromform->completiongradeitemnumber);
 
             if (isset($fromform->cmidnumber)) { //label
                 // set cm idnumber
