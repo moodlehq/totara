8dec19c69e52c5bc68aa4ceda37b4feab07f6b40
Alastair Munro <alastair.munro@totaralms.com>
2012-01-20 14:54:40 +1300

    Reverted part of "T-6907 local/dashboard: Create dashboards and quicklinks block"
    This reverts the moodle core parts of commit b369e003ceefa8676537c09d0e089ebe870238de
    
    Change-Id: I362134efb2e9efc465c0c11da935986371c722fa

--- lib/blocklib.php
+++ lib/blocklib.php
@@ -10,7 +10,6 @@ define('BLOCK_CONFIGURE',   0x10);
 
 define('BLOCK_POS_LEFT',  'l');
 define('BLOCK_POS_RIGHT', 'r');
-define('BLOCK_POS_CENTER', 'c');
 
 define('BLOCKS_PINNED_TRUE',0);
 define('BLOCKS_PINNED_FALSE',1);
