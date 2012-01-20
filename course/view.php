baf171ffe151b5ff3e99fbb33e7fa622da0db4f9
Simon Coggins <simon.coggins@totaralms.com>
2012-01-20 15:22:14 +1300

    Reverts part of "T-5701 Devplan enrol plugin"
    Reverts part of commit aecfb498494870c7264006c6795d0fc25e330d5e
    
    Change-Id: Ib5876079a8739a11edf2f8ee315b4ade5682742b

--- course/view.php
+++ course/view.php
@@ -48,7 +48,7 @@
     if ($switchrole == 0 && confirm_sesskey()) {
         role_switch($switchrole, $context);
     }
-    $coursenotices = array();
+
     require_login($course);
 
     // Switchrole - sanity check in cost-order...
@@ -196,13 +196,8 @@
 
     $CFG->blocksdrag = $useajax;   // this will add a new class to the header so we can style differently
 
-    if (!empty($USER->access['tempra'][$context->path])) {
-        $enrolurl = $CFG->wwwroot . '/course/enrol.php?id=' . $course->id;
-        $noticemsg = get_string('viewingasguest', 'view', $enrolurl);
-        array_push($coursenotices, $noticemsg);
-    }
+
     $PAGE->print_header(get_string('course').': %fullname%', NULL, '', $bodytags);
-    $PAGE->print_notices($coursenotices);
     $completion=new completion_info($course);
     if($completion->is_enabled() && ajaxenabled()) {
         require_js(array('yui_yahoo','yui_event','yui_connection','yui_dom'));
