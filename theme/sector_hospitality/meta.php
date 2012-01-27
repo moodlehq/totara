fc85425376bff72af508377f58c5e38726a3c35e
Aaron Barnes <aaron.barnes@totaralms.com>
2012-01-27 14:35:58 +1300

    Remove Totara themes (need to be rewritten for 2.2)

--- theme/sector_hospitality/meta.php
+++
@@ -1,3 +0,0 @@
-<!--[if IE 6]>
-    <link rel="stylesheet" type="text/css" href="<?php echo $CFG->httpsthemewww ?>/sector_hospitality/styles_ie6.css" />
-<![endif]-->
--- theme/totara_install/meta.php
+++
@@ -1,3 +0,0 @@
-<!--[if IE 6]>
-    <link rel="stylesheet" type="text/css" href="<?php echo $CFG->httpsthemewww ?>/totara_install/styles_ie6.css" />
-<![endif]-->
