44db079b1aeba1b081afa7a322a942b8d6a2b132
Alastair Munro <alastair.munro@totaralms.com>
2012-02-03 12:04:39 +1300

    T-9587 mod/hotpot: Add Moodle 2 version of hotpot module
    Change-Id: I948fb8a7672b7ef8415561bd0d6ab59a120cc2fe
    Reviewed-on: https://review.totaralms.com/1466
    Tested-by: Alastair Munro <alastair.munro@totaralms.com>
    Reviewed-by: Aaron Barnes <aaron.barnes@totaralms.com>

---
+++ mod/hotpot/readme.txt
@@ -0,0 +1,2 @@
+For overview and installation instructions please see:
+http://docs.moodle.org/22/en/Hotpot
---
+++ mod/hotpot/version.php
@@ -0,0 +1,34 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Defines the version of hotpot
+ *
+ * This code fragment is called by moodle_needs_upgrading() and
+ * /admin/index.php
+ *
+ * @package   mod-hotpot
+ * @copyright 2010 Gordon Bateson <gordon.bateson@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$module->version  = 2010080328;
+$module->release  = 'v3.0.28';
+$module->requires = 2010112400;  // Requires this Moodle version
+//$module->cron     = 60;
