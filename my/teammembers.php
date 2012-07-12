7ebc46d1732e59b8475a4b3037ade21dc39df17f
Alastair Munro <alastair.munro@totaralms.com>
2012-07-12 15:05:52 +1200

    T-9655: Add filters to my teammembers embedded report
    Change-Id: Ifee2a4ee10161d212855c5a0309e1a7e1803e820
    Reviewed-on: https://review.totaralms.com/2026
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Yuliya Bozhko <yuliya.bozhko@totaralms.com>
    Reviewed-by: Simon Coggins <simon.coggins@totaralms.com>

--- my/teammembers.php
+++ my/teammembers.php
@@ -36,9 +36,13 @@ require_once($CFG->dirroot.'/totara/reportbuilder/lib.php');
 require_once($CFG->dirroot.'/totara/plan/lib.php');
 
 require_login();
+$PAGE->set_context(context_system::instance());
+$PAGE->set_pagelayout('noblocks');
+$PAGE->set_url(new moodle_url('/my/teammembers.php'));
 
 global $SESSION,$USER;
 
+$format = optional_param('format', '', PARAM_TEXT); // export format
 
 /**
  * Define the "Team Members" embedded report
@@ -50,7 +54,18 @@ if (!$report = reportbuilder_get_embedded_report($shortname)) {
     print_error('error:couldnotgenerateembeddedreport', 'totara_reportbuilder');
 }
 
+$query_string = !empty($_SERVER['QUERY_STRING']) ? '?'.$_SERVER['QUERY_STRING'] : '';
+$log_url = 'bookings.php'.$query_string;
+
+if ($format != '') {
+    add_to_log(SITEID, 'my', 'teammembers export', $log_url, $report->fullname);
+    $report->export_data($format);
+    die;
+}
+
 add_to_log(SITEID, 'my', 'teammembers report view', 'teammembers.php');
+
+$report->include_js();
 
 /**
  * End of defining the report
@@ -64,15 +79,13 @@ add_to_log(SITEID, 'my', 'teammembers report view', 'teammembers.php');
 $staff_records = $DB->get_field('report_builder', 'id', array('shortname' => 'staff_learning_records'));
 $staff_f2f = $DB->get_field('report_builder', 'id', array('shortname' => 'staff_facetoface_sessions'));
 
-$PAGE->set_context(context_system::instance());
-$PAGE->set_pagelayout('noblocks');
-$PAGE->set_url(new moodle_url('/my/teammembers.php'));
 $PAGE->set_totara_menu_selected('myteam');
 $PAGE->set_button($report->edit_button());
 $PAGE->set_title($strheading);
 $PAGE->set_heading($strheading);
 $PAGE->navbar->add(get_string('mylearning', 'totara_core'), new moodle_url('/my/'));
 $PAGE->navbar->add($strheading);
+$renderer = $PAGE->get_renderer('totara_reportbuilder');
 echo $OUTPUT->header();
 
 // Plan menu
@@ -81,10 +94,23 @@ echo dp_display_plans_menu(0,0,'manager', null, null, false);
 // Plan page content
 echo $OUTPUT->container_start('', 'dp-plan-content');
 
-echo html_writer::tag('p', get_string('teammembers_text', 'totara_core'));
+$countfiltered = $report->get_filtered_count();
+$countall = $report->get_full_count();
 
-$report->include_js();
-$report->display_table();
+$heading = $strheading . ': ' . $renderer->print_result_count_string($countfiltered, $countall);
+echo $OUTPUT->heading($heading);
+
+echo html_writer::tag('p', get_string('teammembers_text', 'totara_core'));
+$report->display_search();
+
+echo html_writer::empty_tag('br');
+
+if ($countfiltered>0) {
+    $report->display_table();
+    print $report->edit_button();
+    // export button
+    $renderer->export_select($report->_id);
+}
 
 echo $OUTPUT->container_end();
 echo $OUTPUT->footer();
