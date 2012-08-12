53955c15ded992249008fab86e31a14459e4080b
David Curry <david.curry@totaralms.com>
2012-08-13 11:57:51 +1200

    T-9944: mod/facetoface/: Ported the simple tests to Phpunit
    Change-Id: I408550233dbad5ca353caacd1106385a71fbf524
    Reviewed-on: https://review.totaralms.com/2985
    Reviewed-by: Alastair Munro <alastair.munro@totaralms.com>
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Yuliya Bozhko <yuliya.bozhko@totaralms.com>

--- lib/moodlelib.php
+++ lib/moodlelib.php
@@ -5360,8 +5360,7 @@ function get_mailer($action='get') {
  * @return bool Returns true if mail was sent OK and false if there was an error.
  */
 function email_to_user($user, $from, $subject, $messagetext, $messagehtml='', $attachment='', $attachname='', $usetrueaddress=true, $replyto='', $replytoname='', $wordwrapwidth=79) {
-
-    global $CFG;
+    global $CFG, $FULLME, $UNITTEST;
 
     // TODO: use ical library instead
     if (substr($messagehtml, 0, 5) == 'ical:') {
@@ -5396,7 +5395,7 @@ function email_to_user($user, $from, $subject, $messagetext, $messagehtml='', $a
         // hidden setting for development sites, set in config.php if needed
         $noemail = 'Not sending email due to noemailever config setting';
         error_log($noemail);
-        if (CLI_SCRIPT) {
+        if (CLI_SCRIPT && empty($UNITTEST->running)) {
             mtrace('Error: lib/moodlelib.php email_to_user(): '.$noemail);
         }
         return true;
