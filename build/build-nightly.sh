db9d89816e5a8e61829836b4833a77eb95c67ed5
Simon Coggins <simon.coggins@totaralms.com>
2012-08-20 15:14:18 +1200

    T-6080: Update to latest 2.x version of link checker
    Change-Id: I6660cceb9cb8e7e34e8affd2cac4274939fe9f49
    Reviewed-on: https://review.totaralms.com/2268
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Simon Coggins <simon.coggins@totaralms.com>

--- build/build-nightly.sh
+++ build/build-nightly.sh
@@ -48,6 +48,9 @@ then
     cd -
 fi
 
-# TODO reenable once link_checker ported to work with 2.2
-#echo "STEP 6: Run link checker script as a learner";
-#sudo -u www-data php build/link_checker.php $1 learner 'passworD1!'
+echo "STEP 6: Run link checker script as a learner";
+sudo -u www-data php build/link_checker.php $1 learner 'passworD1!'
+
+echo "STEP 7: Run link checker script as an admin";
+sudo -u www-data php build/link_checker.php $1 admin 'passworD1!'
+
