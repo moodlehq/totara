d57d011ad8d75d3930716cefdb3a31cac50afec9
Ciaran Irvine <ciaran.irvine@totaralms.com>
2012-08-24 13:52:34 +1200

    T-9804 totara/message: Port language strings to 2.2
    Change-Id: I33c3bfb4da5db5873f73ea315d2d3af91e70e1a3
    Reviewed-on: https://review.totaralms.com/2317
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: David Curry <david.curry@totaralms.com>
    Reviewed-by: Simon Coggins <simon.coggins@totaralms.com>

---
+++ totara/message/rb_sources/lang/hu/rb_source_totaramessages.php
@@ -0,0 +1,27 @@
+<?php
+// rb_source_totaramessages.php - created with Totara langimport script version 1.1
+
+$string['actions'] = 'Műveletek';
+$string['completedorg'] = 'A szervezeti egység, amikor elkészült';
+$string['currentorg'] = 'A felhasználó jelenlegi szervezeti egysége';
+$string['currentpos'] = 'A felhasználó jelenlegi munkaköre';
+$string['date'] = 'Teljesítési dátum';
+$string['datesent'] = 'Küldési dátum';
+$string['details'] = 'Részletek';
+$string['dismissmsg'] = 'Üzenet elvetése';
+$string['msgcategory'] = 'Üzenet kategória';
+$string['msgid'] = 'Üzenet azonosító';
+$string['msgstatus'] = 'Üzenet állapot';
+$string['msgtype'] = 'Üzenet típus';
+$string['msgurgency'] = 'Üzenet sürgősség';
+$string['name'] = 'Név';
+$string['select'] = 'Kiválasztás';
+$string['sent'] = 'Küldött';
+$string['sourcetitle'] = 'Üzenetek';
+$string['statement'] = 'Utasítás';
+$string['statementurl'] = 'Utasítás (hivatkozás az áttekintésre)';
+$string['type'] = 'Típus';
+$string['type_message_values'] = 'Üzenet értékek';
+$string['user'] = 'A felhasználó';
+
+?>
---
+++ totara/message/rb_sources/lang/pl/rb_source_totaramessages.php
@@ -0,0 +1,27 @@
+<?php
+// rb_source_totaramessages.php - created with Totara langimport script version 1.1
+
+$string['actions'] = 'Czynności';
+$string['completedorg'] = 'Organizacja w chwili ukońćzenia';
+$string['currentorg'] = 'Bieżąca organizacja użytkownika';
+$string['currentpos'] = 'Bieżące stanowisko użytkownika';
+$string['date'] = 'Data ukończenia';
+$string['datesent'] = 'Data wysłania';
+$string['details'] = 'Szczegóły';
+$string['dismissmsg'] = 'Odrzuć wiadomość';
+$string['msgcategory'] = 'Kategoria wiadomości';
+$string['msgid'] = 'ID wiadomości';
+$string['msgstatus'] = 'Stan wiadomości';
+$string['msgtype'] = 'Typ wiadomości';
+$string['msgurgency'] = 'Priorytet wiadomości';
+$string['name'] = 'Nazwa';
+$string['select'] = 'Wybierz';
+$string['sent'] = 'Wysłane';
+$string['sourcetitle'] = 'Wiadomości';
+$string['statement'] = 'Oświadczenie';
+$string['statementurl'] = 'Oświadczenie (powiązanie z przeglądem)';
+$string['type'] = 'Typ';
+$string['type_message_values'] = 'Wartości wiadomości';
+$string['user'] = 'Użytkownik';
+
+?>
