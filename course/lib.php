baed3052e4ee2beb185bc0b325d51f389719f57c
David Curry <david.curry@totaralms.com>
2012-09-04 14:14:20 +1200

    T-9898 lang/: changed myHome to myLearning for consistency.
    Change-Id: I2a9a0df737c481683f4764c0ff63484de31ff5ca
    Reviewed-on: https://review.totaralms.com/2358
    Reviewed-by: Alastair Munro <alastair.munro@totaralms.com>
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Yuliya Bozhko <yuliya.bozhko@totaralms.com>

--- course/lib.php
+++ course/lib.php
@@ -2583,7 +2583,7 @@ function print_my_moodle() {
     global $USER, $CFG, $DB, $OUTPUT;
 
     if (!isloggedin() or isguestuser()) {
-        print_error('nopermissions', '', '', 'See My Home');
+        print_error('nopermissions', '', '', 'See My Learning');
     }
 
     $courses  = enrol_get_my_courses('summary', 'visible DESC,sortorder ASC');
