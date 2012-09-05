425ada94e4bd505488c5bf215b728ea3e834e362
Ciaran Irvine <ciaran.irvine@totaralms.com>
2012-09-05 16:37:03 +1200

    T-7189 version.php: Tagging version 2.2.3
    Change-Id: Id0c4d23141cbf6d6219b4354d2ee26a2ac9b9d35
    Reviewed-on: https://review.totaralms.com/2361
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Simon Coggins <simon.coggins@totaralms.com>

--- version.php
+++ version.php
@@ -45,6 +45,6 @@ $maturity = MATURITY_STABLE;            // this version's maturity level
 // This can be used for modules to set a minimum functionality requirement.
 
 $TOTARA = new stdClass();
-$TOTARA->version    = '2.2.2';              # Please keep as string
-$TOTARA->build      = '20120829.00';        # Please keep as string
+$TOTARA->version    = '2.2.3';              # Please keep as string
+$TOTARA->build      = '20120905.00';        # Please keep as string
 $TOTARA->release    = "{$TOTARA->version} (Build: {$TOTARA->build})";
