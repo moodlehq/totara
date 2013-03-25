1350cd133c626219cf853c5238b1aa645b6be9b2
David Curry <david.curry@totaralms.com>
2013-03-25 17:02:11 +1300

    T-10408 totara_sync: Add clear buttons to Totara Sync log
    Change-Id: Iba9ecf5a1a8526e46f0e1a749f125f5b974742a6
    Reviewed-on: https://review.totaralms.com/3028
    Reviewed-by: Ciaran Irvine <ciaran.irvine@totaralms.com>
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Simon Coggins <simon.coggins@totaralms.com>

--- admin/tool/totara_sync/admin/synclog.php
+++ admin/tool/totara_sync/admin/synclog.php
@@ -24,13 +24,16 @@
 
 require_once('../../../../config.php');
 require_once($CFG->dirroot . '/totara/reportbuilder/lib.php');
+require_once($CFG->dirroot . '/' . $CFG->admin . '/tool/totara_sync/lib.php');
 
 $debug  = optional_param('debug', false, PARAM_BOOL);
 $format = optional_param('format', '', PARAM_TEXT); // export format
+$delete = optional_param('del', 'none', PARAM_ALPHANUM);
 
 $context = context_system::instance();
 require_capability('tool/totara_sync:manage', $context);
 $PAGE->set_context($context);
+$PAGE->set_url('/' . $CFG->admin . '/tool/totara_sync/admin/synclog.php');
 
 if ($CFG->forcelogin) {
     require_login();
@@ -42,6 +45,36 @@ $shortname = 'totarasynclog';
 
 if (!$report = reportbuilder_get_embedded_report($shortname)) {
     print_error('error:couldnotgenerateembeddedreport', 'totara_reportbuilder');
+}
+
+if ($delete != 'none' && confirm_sesskey()) {
+    if (has_capability('tool/totara_sync:deletesynclog', $context)) {
+        $sql = 'SELECT MAX(time) as maxtime from {totara_sync_log}';
+        $maxtime = $DB->get_field_sql($sql);
+        $runid = latest_runid();
+
+        if ($delete == 'all' || $delete == 'partial') {
+            //double check before deleting (almost) all the records from totara_sync_log
+            $confirmed = new moodle_url('/' . $CFG->admin . '/tool/totara_sync/admin/synclog.php',
+                array('del' => md5($maxtime . ':' . $delete . ':' . $runid)));
+            $cancelled = new moodle_url('/' . $CFG->admin . '/tool/totara_sync/admin/synclog.php');
+
+            echo $OUTPUT->header();
+            echo $OUTPUT->confirm(get_string("delete{$delete}synclogcheck", 'tool_totara_sync'), $confirmed, $cancelled);
+            echo $OUTPUT->footer();
+            exit;
+        } else {
+            if ($delete == md5($maxtime . ':' . 'all' . ':' . $runid)) {
+                // delete all sync
+                $DB->delete_records('totara_sync_log');
+            } else if ($delete == md5($maxtime . ':' . 'partial' . ':' . $runid)) {
+                // delete all but most recent sync
+                $DB->delete_records_select('totara_sync_log', 'runid < ?', array('runid' => $runid));
+            }
+        }
+    } else {
+        print_error('error:deletesynclogpermission', 'tool_totara_sync');
+    }
 }
 
 if ($format != '') {
@@ -59,7 +92,6 @@ $report->include_js();
 $fullname = format_string($report->fullname);
 $pagetitle = format_string(get_string('report', 'totara_core') . ': ' . $fullname);
 
-$PAGE->set_url('/admin/tool/totara_sync/admin/synclog.php');
 $PAGE->set_pagelayout('admin');
 $PAGE->navbar->add(get_string('view'));
 $PAGE->set_title($pagetitle);
@@ -83,6 +115,11 @@ if ($countfiltered > 0) {
 
     // export button
     $renderer->export_select($report->_id);
+}
+
+if (has_capability('tool/totara_sync:deletesynclog', $context)) {
+    echo $OUTPUT->single_button(new moodle_url('/' . $CFG->admin . '/tool/totara_sync/admin/synclog.php', array('del' => 'all')), get_string('deleteallsynclog', 'tool_totara_sync'));
+    echo $OUTPUT->single_button(new moodle_url('/' . $CFG->admin . '/tool/totara_sync/admin/synclog.php', array('del' => 'partial')), get_string('deletepartialsynclog', 'tool_totara_sync'));
 }
 
 echo $OUTPUT->footer();
--- admin/tool/totara_sync/lang/en/tool_totara_sync.php
+++ admin/tool/totara_sync/lang/en/tool_totara_sync.php
@@ -36,6 +36,7 @@ $string['totara_sync:managepos'] = 'Manage Totara sync positions';
 $string['totara_sync:uploaduser'] = 'Upload Totara sync users';
 $string['totara_sync:uploadorg'] = 'Upload Totara sync organisations';
 $string['totara_sync:uploadpos'] = 'Upload Totara sync positions';
+$string['totara_sync:deletesynclog'] = 'Clear the sync logs';
 $string['settingssaved'] = 'Settings saved';
 $string['elementenabled'] = 'Element enabled';
 $string['elementdisabled'] = 'Element disabled';
@@ -234,6 +235,12 @@ $string['nosourceenabled'] = 'No source enabled for this element.';
 $string['syncexecute'] = 'Run Sync';
 $string['runsynccronstart'] = 'Running totara_sync cron...';
 $string['runsynccronend'] = 'Done!';
+$string['deleteallsynclog'] = 'Clear all records';
+$string['deletepartialsynclog'] = 'Clear all except latest records';
+$string['deleteallsynclogcheck'] = 'Are you absolutely sure you want to delete all the Totara Sync log records?';
+$string['deletepartialsynclogcheck'] = 'Are you absolutely sure you want to delete all the Totara Sync log records except for the most recent run?';
+$string['error:deletesynclogpermission'] = 'You do not have permission to delete sync log records!';
+
 ///
 /// Totara sync log reports
 ///
