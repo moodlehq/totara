db87dc8546da5861515805ed61aff5ad7a2787f5
Oleg Demeshev <oleg.demeshev@totaralms.com>
2013-05-14 11:08:47 +1200

    T-10616 mod/facetoface/lib.php: Add style to 'Cancel booking' to consist with other links
    Change-Id: I6b6a139669aaf174bd32caf60de4c84646d40fdd
    Reviewed-on: https://review.totaralms.com/3244
    Reviewed-by: Yuliya Bozhko <yuliya.bozhko@totaralms.com>
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Nathan Lewis <nathan.lewis@totaralms.com>

--- mod/facetoface/lib.php
+++ mod/facetoface/lib.php
@@ -2242,7 +2242,7 @@ function facetoface_print_coursemodule_info($coursemodule) {
             if (!facetoface_has_session_started($session, $timenow)) {
                 $strcancelbooking = get_string('cancelbooking', 'facetoface');
                 $cancel_url = new moodle_url('/mod/facetoface/cancelsignup.php', array('s' => $session->id));
-                $cancellink = html_writer::tag('tr', html_writer::tag('td', html_writer::link($cancel_url, $strcancelbooking, array('title' => $strcancelbooking))));
+                $cancellink = html_writer::tag('tr', html_writer::tag('td', html_writer::link($cancel_url, $strcancelbooking, array('class' => 'f2fsessionlinks f2fviewallsessions', 'title' => $strcancelbooking))));
             }
 
             $strmoreinfo = get_string('moreinfo', 'facetoface');
