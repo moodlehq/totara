0a6d73fdd5e901e05fda518e7a62ba518a963b38
Simon Coggins <simon.coggins@totaralms.com>
2013-05-14 15:20:04 +1200

    T-7189 version.php: Tagging version 2.4.1
    Change-Id: Id815ee7905b8d61f3a81393bc2ec03c35e0e2f74
    Reviewed-on: https://review.totaralms.com/3247
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Simon Coggins <simon.coggins@totaralms.com>
    Reviewed-by: Nathan Lewis <nathan.lewis@totaralms.com>

--- CHANGELOG.php
+++ CHANGELOG.php
@@ -3,6 +3,66 @@
 
 Totara LMS Changelog
 
+Release 2.4.1 (14th May 2013):
+==================================================
+
+New features:
+
+    T-9460      Add learning plan report source
+
+Improvements:
+
+    T-10193     Set minimum width on tinymce editors
+    T-10726     Change the My Team page to be editable, so blocks can be added
+    T-10742     Add module requirements to completion progress details page
+    T-10316     Show warning message if session is overbooked
+    T-10697     Display end dates of the Facetoface sessions over multiple dates
+    T-10662     Add firstaccess column to a user report source
+    T-10573     Allow embedding media in program summary and endnote
+    T-10713     Move changelog into PHP file to prevent version identification via the web
+    T-10702     Show email next to user name when enrolling individuals
+
+Database Upgrades:
+
+
+Bug Fixes:
+
+    T-10663     Allow selected scheduling conflicts doesn't work from bulk uploads
+    T-10694     Ensure a table exists before trying to rename it
+    T-10416     Fix rules issues in cloned audiences
+    T-10669     Fix broken links to course reports
+    T-10728     Fix incorrect calculation of recurrence time for recurring courses in programs
+    T-10659     Ensure face-to-face signup page checks for date conflicts
+    T-10739     Face-to-face session time is shifted by 12 hours in UTC zone
+    T-10732     Fix manager unassignment when manager is changed
+    T-10708     Fix sessions dates and times display
+    T-10680     Hierarchy textarea custom fields break report builder layout in MSSQL
+    T-10158     Fix show/hide columns status in reports
+    T-10539     Fixed character encoding issues in pdf certificates
+    T-10709     Fix for facetoface iCals containing double quotes
+    T-10693     Handle missing manager on user import in totara_sync
+    T-10689     Set user specified session duration if unknown datetime in Facetoface
+    T-10706     Fix fatal error when installing in a non-English language with no predefined config.php
+    T-10722     Fix warning message when saving backpack settings without groups
+    T-10711     Fix for carriage returns in bulk uploads in Facetoface sessions
+    T-10650     Filter by Message Type doesn't work in MySQL for messages report
+    T-10515     Remove "Stay on this page" prompt when trying to confirm audience rule changes
+    T-10567     Error when trying to cache some reports due to "Column length too big for column"
+    T-10373     Redirect to courses page if unable to enrol in a course
+    T-10380     Fix status completion required when updating scorm
+    T-10705     Fix potential duplicated columns in synclog
+    T-10352     Set correct user in createdby field of signup status updates
+    T-10361     Show sidebar on Learning plan template component tab
+    T-10701     Fix error when upgrading to 2.4.0 if sessions have 'Approval required' unchecked
+    T-10507     On programs page, "turn editing on/off" redirects to course page, not program page
+    T-10277     Fix the amount of days in a year in programs
+    T-9446      Fix facetoface sessions duration output
+    T-10660     Face-to-face session
+    T-10574     Fix display of images in program description
+    T-10670     Prevent Notifications menu item appearing in face-to-face navigation for learner
+    T-10490     Fix layout of minimise and dock icons in blocks
+
+
 Release 2.4.0 (30 April 2013):
 ==================================================
 
