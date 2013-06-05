6e850c724dd52534d3b9a92baebc7d2a99dc799b
Moises Burgos <moises.burgos@totaralms.com>
2013-06-06 11:13:15 +1200

    T-10801 general: Fix spacing in user's profile
    Change-Id: I40f516ee191a2986d392d1b2212ffb23e6c00628
    Reviewed-on: https://review.totaralms.com/3370
    Reviewed-by: Yuliya Bozhko <yuliya.bozhko@totaralms.com>
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Nathan Lewis <nathan.lewis@totaralms.com>

--- user/profile/lib.php
+++ user/profile/lib.php
@@ -459,16 +459,16 @@ function profile_display_hierarchy_fields($userid) {
     $record = $DB->get_record_sql($sql, array($userid), IGNORE_MULTIPLE);
 
     if (isset($record->pos)) {
-        print_row(get_string('position', 'totara_hierarchy') . " :", $record->pos);
+        print_row(get_string('position', 'totara_hierarchy') . ":", $record->pos);
     }
 
     if (isset($record->org)) {
-        print_row(get_string('organisation', 'totara_hierarchy') . " :", $record->org);
+        print_row(get_string('organisation', 'totara_hierarchy') . ":", $record->org);
     }
 
     if (isset($record->manid)) {
         $manurl = html_writer::link(new moodle_url('/user/profile.php', array("id" => $record->manid)), $record->fullname);
-        print_row(get_string('manager', 'totara_hierarchy') . " :", $manurl);
+        print_row(get_string('manager', 'totara_hierarchy') . ":", $manurl);
     }
 }
 
