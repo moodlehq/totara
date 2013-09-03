d7eca64e6018d8ffd61b3f89b891be5b1c8e4745
Moises Burgos <moises.burgos@totaralms.com>
2013-09-03 17:32:55 +1200

    T-7189 version.php: Bumping version 2.4.9+
    Change-Id: I6e0eddf96ea8b9aa40d04f8e530594e996cc7142
    Reviewed-on: https://review.totaralms.com/3698
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Yuliya Bozhko <yuliya.bozhko@totaralms.com>
    Reviewed-by: Simon Coggins <simon.coggins@totaralms.com>

--- version.php
+++ version.php
@@ -45,7 +45,7 @@ $maturity = MATURITY_STABLE;            // this version's maturity level
 // This can be used for modules to set a minimum functionality requirement.
 
 $TOTARA = new stdClass();
-$TOTARA->version    = '2.4.9';         // Please keep as string.
+$TOTARA->version    = '2.4.9+';        // Please keep as string.
 $TOTARA->build      = '20130903.00';   // Please keep as string.
 $TOTARA->release    = "{$TOTARA->version} (Build: {$TOTARA->build})";
 
