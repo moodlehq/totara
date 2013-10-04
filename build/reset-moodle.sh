f05910e427521f7546993f49bd61eaa748d1134e
Simon Coggins <simon.coggins@totaralms.com>
2013-10-04 15:23:59 +1300

    T-6080: Abstract branchname out of moodle reset script
    Change-Id: I664046953191d53197135df0e252a94903bab03b
    Reviewed-on: https://review.totaralms.com/3845
    Tested-by: Simon Coggins <simon.coggins@totaralms.com>
    Reviewed-by: Simon Coggins <simon.coggins@totaralms.com>

--- build/reset-moodle.sh
+++ build/reset-moodle.sh
@@ -3,6 +3,7 @@
 # This accepts three arguments
 # 1/ The database server e.g. oak
 # 2/ The database type e.g. postgres7
+# 3/ The branch name e.g. t2-release-2.5
 
 echo "Update Jenkins directory permissions"
 # when Jenkins updates via apt it resets to 750 which, makes the webroot
@@ -45,11 +46,13 @@ else
     DBTYPE="$2"
 fi
 
+BRANCHNAME=$3
+
 # Make sure we have the latest Totara stuff.
 git fetch origin
 
 # Get the moodle version out of the version.php file.
-HASH=`git rev-parse origin/t2-release-2.4`
+HASH=`git rev-parse origin/${BRANCHNAME}`
 # this line gets the version file from the latest patchset of the branch
 # then finds the release line
 # then replaces the whole line with just the version number (e.g. 2.4.5)
@@ -86,8 +89,8 @@ echo "\$CFG->phpunit_dataroot = '/var/lib/jenkins/jobs/$JOB_NAME/phpunit_dataroo
 
 # TODO - do we want to put anything in here or are we good with upgrading a fresh/empty install?
 
-git checkout t2-release-2.4
-git reset --hard origin/t2-release-2.4
+git checkout ${BRANCHNAME}
+git reset --hard origin/${BRANCHNAME}
 
 echo "Upgrade to Totara"
 sudo -u www-data php /var/lib/jenkins/jobs/${JOB_NAME}/workspace/admin/cli/upgrade.php --non-interactive
