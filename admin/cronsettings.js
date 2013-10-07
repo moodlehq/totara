b2e20d9bab484314d783f4975ae4e68253ce8778
Nathan Lewis <nathan.lewis@totaralms.com>
2013-10-08 12:18:35 +1300

    T-11420 general: Fix execute and refresh buttons on cron web interface in IE8
    Change-Id: I83fa6bd4ea28ddaee67352db3363167651c66a28
    Reviewed-on: https://review.totaralms.com/3866
    Reviewed-by: Yuliya Bozhko <yuliya.bozhko@totaralms.com>
    Reviewed-by: David Curry <david.curry@totaralms.com>
    Tested-by: Yuliya Bozhko <yuliya.bozhko@totaralms.com>

--- admin/cronsettings.js
+++ admin/cronsettings.js
@@ -21,6 +21,34 @@
  * @package totara
  * @subpackage cron
  */
+
+// Fix for missing bind function in < IE9.
+if (Y.UA.ie < 9) {
+    if (!Function.prototype.bind) {
+      Function.prototype.bind = function (oThis) {
+        if (typeof this !== "function") {
+          // Closest thing possible to the ECMAScript 5 internal IsCallable function.
+          throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+        }
+
+        var aArgs = Array.prototype.slice.call(arguments, 1),
+            fToBind = this,
+            fNOP = function () {},
+            fBound = function () {
+              return fToBind.apply(this instanceof fNOP && oThis
+                                     ? this
+                                     : oThis,
+                                   aArgs.concat(Array.prototype.slice.call(arguments)));
+            };
+
+        fNOP.prototype = this.prototype;
+        fBound.prototype = new fNOP();
+
+        return fBound;
+      };
+    }
+}
+
 M.cronsettings = M.cronsettings || {
     // optional php params and defaults defined here, args passed to init method
     // below will override these values
