a4d4a9feedda5d345f1bf014c94fea0dd39c127e
Yuliya Bozhko <yuliya.bozhko@totaralms.com>
2013-10-15 17:15:47 +1300

    T-11317 admin: Remove admin/report/*
    Change-Id: I7ad0de618ce484f907030953ee41ec86d74ae270
    Reviewed-on: https://review.totaralms.com/3928
    Reviewed-by: Alastair Munro <alastair.munro@totaralms.com>
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: David Curry <david.curry@totaralms.com>

--- admin/report/spamcleaner/settings.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-// spam cleaner
-$ADMIN->add('reports', new admin_externalpage('reportspamcleaner', get_string('spamcleaner','report_spamcleaner'), "$CFG->wwwroot/$CFG->admin/report/spamcleaner/index.php", 'moodle/site:config'));
-
--- admin/report/stats/settings.php
+++
@@ -1,4 +0,0 @@
-<?php  // $Id$
-// just a link to course report
-$ADMIN->add('reports', new admin_externalpage('reportstats', get_string('stats', 'admin'), "$CFG->wwwroot/course/report/stats/index.php", 'coursereport/stats:view'));
-?>
