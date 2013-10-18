0aa8fc29ee652c25cc7d64fb83c8a810f5298e4b
David Curry <david.curry@totaralms.com>
2013-10-18 15:47:42 +1300

    T-11522 core: Remove old duplicate 1.1 event handlers
    Change-Id: I0b3319ae529690cf32d5b3081227005037424923
    Reviewed-on: https://review.totaralms.com/3960
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Simon Coggins <simon.coggins@totaralms.com>
    Reviewed-by: Oleg Demeshev <oleg.demeshev@totaralms.com>

--- totara/core/db/upgrade.php
+++ totara/core/db/upgrade.php
@@ -866,5 +866,31 @@ function xmldb_totara_core_upgrade($oldversion) {
         totara_upgrade_mod_savepoint(true, 2013101100, 'totara_core');
     }
 
+    // Remove duplicate 1.1 legacy event handlers.
+    if ($oldversion < 2013101800) {
+        $handlers = array(
+            'local_cohort#organisation_deleted',
+            'local_cohort#organisation_updated',
+            'local_cohort#position_deleted',
+            'local_cohort#position_updated',
+            'local_cohort#profilefield_deleted',
+            'local_program#program_assigned',
+            'local_program#program_completed',
+            'local_program#program_courseset_completed',
+            'local_program#program_unassigned',
+            'local_program#user_firstaccess',
+            'local#role_assigned',
+            'local#user_deleted',
+        );
+
+        // Delete the outdated handlers.
+        foreach ($handlers as $handler) {
+            $hinfo = explode('#', $handler);
+            $DB->delete_records('events_handlers', array('component' => $hinfo[0], 'eventname' => $hinfo[1]));
+        }
+
+        totara_upgrade_mod_savepoint(true, 2013101800, 'totara_core');
+    }
+
     return true;
 }
