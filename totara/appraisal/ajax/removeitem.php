ff7fb3fc0a4407fb9c44f489a752ed0bc54d43a4
Nathan Lewis <nathan.lewis@totaralms.com>
2013-10-18 14:52:48 +1300

    T-11525 appraisals: Fix error removing review items when appraisal only has one role
    Change-Id: Iab871dfd575b27d861a8856b6016e884569d0c17
    Reviewed-on: https://review.totaralms.com/3958
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Alastair Munro <alastair.munro@totaralms.com>
    Reviewed-by: Simon Coggins <simon.coggins@totaralms.com>

--- totara/appraisal/ajax/removeitem.php
+++ totara/appraisal/ajax/removeitem.php
@@ -46,27 +46,37 @@ if (!$appraisal->can_access($roleassignment)) {
     throw new moodle_exception('error:cannotaccessappraisal');
 }
 
+// Check if other roles have already provided answers, preventing the deletion.
 $roles = appraisal::get_related_roleassignmentids($reviewdata->appraisalroleassignmentid);
 unset($roles[$reviewdata->appraisalroleassignmentid]);
-list($rolessql, $roleids) = $DB->get_in_or_equal($roles);
+if (!empty($roles)) {
+    list($rolessql, $roleids) = $DB->get_in_or_equal($roles);
 
-$sql = "SELECT *
-          FROM {appraisal_review_data}
-         WHERE itemid = ?
-           AND appraisalquestfieldid = ?
-           AND appraisalroleassignmentid " . $rolessql . "
-           AND NOT " . $DB->sql_isempty('appraisal_review_data', 'content', true, true);
-$params = array_merge(array($reviewdata->itemid, $reviewdata->appraisalquestfieldid), $roleids);
-if ($reviewdata->scope > 0) {
-    $sql .= ' AND scope = ?';
-    $params[] = $reviewdata->scope;
+    $sql = "SELECT *
+              FROM {appraisal_review_data}
+             WHERE itemid = ?
+               AND appraisalquestfieldid = ?
+               AND appraisalroleassignmentid " . $rolessql . "
+               AND NOT " . $DB->sql_isempty('appraisal_review_data', 'content', true, true);
+    $params = array_merge(array($reviewdata->itemid, $reviewdata->appraisalquestfieldid), $roleids);
+
+    if ($reviewdata->scope > 0) {
+        $sql .= ' AND scope = ?';
+        $params[] = $reviewdata->scope;
+    }
+    $otherroleanswers = $DB->get_records_sql($sql, $params);
+} else {
+    $otherroleanswers = array();
 }
-$entries = $DB->get_records_sql($sql, $params);
 
-if (empty($entries)) {
+if (empty($otherroleanswers)) {
     $DB->delete_records('appraisal_review_data', array('itemid' => $reviewdata->itemid, 'scope' => $reviewdata->scope,
-        'appraisalquestfieldid' =>  $reviewdata->appraisalquestfieldid));
+            'appraisalquestfieldid' =>  $reviewdata->appraisalquestfieldid));
     if (is_ajax_request($_SERVER)) {
         echo ('success');
+    }
+} else {
+    if (is_ajax_request($_SERVER)) {
+        echo ('failed - locked because other role has provided answer');
     }
 }
