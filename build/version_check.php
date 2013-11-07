581e336a582b082be4ddc35dbffa69226399d687
Simon Coggins <simon.coggins@totaralms.com>
2013-11-08 10:19:43 +1300

    T-10704 testing: Additional improvements to version number check
    Change-Id: I7266e5764aa6842f78f60fb13a336bd9bcba2130
    Reviewed-on: https://review.totaralms.com/4079
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Yuliya Bozhko <yuliya.bozhko@totaralms.com>
    Reviewed-by: David Curry <david.curry@totaralms.com>

--- build/version_check.php
+++ build/version_check.php
@@ -8,19 +8,17 @@ list($options, $unrecognized) = cli_get_params(array('filepath' => '', 'remote' 
 
 $filepath = clean_param($options['filepath'], PARAM_TEXT);
 $remote = clean_param($options['remote'], PARAM_ALPHANUM);
-$branch = clean_param($options['branch'], PARAM_TEXT);
+$targetbranch = clean_param($options['branch'], PARAM_TEXT);
 
-if (empty($filepath) || empty($remote) || empty($branch)) {
+if (empty($filepath) || empty($remote) || empty($targetbranch)) {
     echo "ERROR: version_check has received empty parameters";
     die();
 }
 
-if (!preg_match('/t[0-9]-release-[0-9]\.[0-9]$/', $branch)) {
-    echo "INFO: '{$branch}' is not a stable release branch. Skipping checks.\n";
+if (!preg_match('/t[0-9]-release-[0-9]\.[0-9]$/', $targetbranch)) {
+    echo "INFO: '{$targetbranch}' is not a stable release branch. Skipping checks.\n";
     die;
 }
-
-echo "Checking version file {$filepath}\n";
 
 // Dummy some Moodle objects
 $plugin = new stdClass();
@@ -43,8 +41,8 @@ $latestbranch = key($branches);
 reset($branches);
 
 // Check the branch passed in url is known.
-if (!isset($branches[$branch])) {
-    echo "ERROR: Invalid remote branch '{$branch}' passed to version_check - known branches in remote '{$remote}':\n" . implode("\n", array_flip($branches)) . "\n";
+if (!isset($branches[$targetbranch])) {
+    echo "ERROR: Invalid remote branch '{$targetbranch}' passed to version_check - known branches in remote '{$remote}':\n" . implode("\n", array_flip($branches)) . "\n";
     die();
 }
 
@@ -113,8 +111,14 @@ if ($versiondata === false) {
     echo "INFO: plugin info missing, skipping rest of tests\n";
     die();
 }
-$component = $versiondata['component'];
-$thisversion = $versiondata['version'];
+
+// If component not stored in version.php, make it up based on file path.
+if ($versiondata['component'] == 'unknown') {
+    $component = str_replace(array('/', '\\'), '_', dirname($filepath));
+} else {
+    $component = $versiondata['component'];
+}
+$thispatchversion = $versiondata['version'];
 
 //get the plugin version in the preceeding release codebase
 //e.g. if tversion is 2.4.2+ then the last release was 2.4.2 and this patch is planned for 2.4.3
@@ -125,7 +129,7 @@ if (empty($gitresult)){
 }
 $versiondata = get_version_number_from_file_array($gitresult);
 if ($versiondata === false) {
-    echo "WARNING: no release plugin version number found in totara-$tversion:$filepath\n";
+    echo "WARNING: could not parse plugin version number in totara-$tversion:$filepath\n";
     die();
 }
 $releaseversion = $versiondata['version'];
@@ -133,21 +137,27 @@ $releaseversion = $versiondata['version'];
 //now the actual tests!
 
 // TEST 1. make sure the new version number is yyyymmddxx format
-test_date_format($component, $thisversion);
+test_date_format($component, $thispatchversion);
 
 // TEST 2. make sure the number is actually incrementing from the existing version
-if ($thisversion < $releaseversion) {
-    echo "ERROR: $component version $thisversion is LOWER then the version in the $tversion release ($releaseversion)\n";
+if ($thispatchversion < $releaseversion) {
+    echo "ERROR: $component version $thispatchversion is LOWER then the version in the $tversion release ($releaseversion)\n";
 }
 
 // TEST 3. if on an old branch, make sure it is a minor bump
-if ($branch != $latestbranch && ($thisversion-$releaseversion) > 99) {
+if ($targetbranch != $latestbranch && ($thispatchversion - $releaseversion) > 99) {
     //we have sometimes in the past had multiple patches all bumping the version on the same component during a release cycle
-    echo "ERROR: Major version bump on old branch! $component version $thisversion\n";
+    echo "ERROR: Major version bump on old branch! $component version $thispatchversion\n";
 }
 
-// TEST 4. check number across all branches that it increments properly
+// TEST 4. if on current branch, make sure it is a major bump.
+if ($targetbranch == $latestbranch && $thispatchversion > $releaseversion && ($thispatchversion - $releaseversion) < 99) {
+    echo "ERROR: Minor version bump on latest branch! $component version $thispatchversion\n";
+}
+
+// TEST 5. check number across all branches that it increments properly
 $lastnumber = 0; $lastbranch = '';
+// $branches runs oldest to newest. Format: $branch = 't2-release-2.2'; $series = '2.2';
 foreach ($branches as $branch => $series) {
     $gitresult = array();
     $gitreturn = "";
@@ -162,23 +172,34 @@ foreach ($branches as $branch => $series) {
 
     $versiondata = get_version_number_from_file_array($gitresult);
     if ($versiondata === false) {
-        echo "WARNING: no branch plugin version number found in $branch:$filepath\n";
+        // We should always be able to determine the version if the file exists.
+        echo "ERROR: no branch plugin version number found in $branch:$filepath\n";
         die();
     }
-    $component = $versiondata['component'];
+
+    // If component not stored in version.php, make it up based on file path.
+    if ($versiondata['component'] == 'unknown') {
+        $component = str_replace(array('/', '\\'), '_', dirname($filepath));
+    } else {
+        $component = $versiondata['component'];
+    }
     $branchversion = $versiondata['version'];
 
-    //if we have something to compare, compare the branch version AND this patch version
-    //make sure thisversion is greater than branchversion and both are higher than lastbranch version
-    if (!empty($lastbranch) && !empty($lastnumber) && (
-            intval($branchversion) > intval($thisversion) ||
-            intval($lastnumber) > intval($branchversion)
-                                                      )) {
+    // If we have something to compare, compare the patch branch version with the last branch version
+    // make sure thispatchversion is greater.
+    if (!empty($lastbranch) && !empty($lastnumber) &&
+            intval($lastnumber) > intval($thispatchversion)) {
+        echo "ERROR: this patch DOWNGRADES version number on $component ($thispatchversion) from $lastbranch ($lastnumber)\n";
+    }
+    // Also make sure that the current branch is greater than the last one.
+    if (!empty($lastbranch) && !empty($lastnumber) &&
+            intval($lastnumber) > intval($branchversion)) {
         echo "ERROR: $branch DOWNGRADES version number on $component ($branchversion) from $lastbranch ($lastnumber)\n";
     }
     $lastnumber = $branchversion;
     $lastbranch = $branch;
 }
+
 
 /**
  * Given the contents of a version.php file as an array (one line per element),
@@ -193,12 +214,22 @@ function get_version_number_from_file_array($file_array) {
         }
         $version_str .= $line . "\n";
     }
+
+    // Initialise module and plugin to stop strict warnings
+    $module = new stdClass();
+    $plugin = new stdClass();
+
     eval($version_str);
 
     if (isset($plugin->component)) {
         return array('component' => $plugin->component, 'version' => $plugin->version);
     } else if (isset($module->component)) {
         return array('component' => $module->component, 'version' => $module->version);
+    } else if (isset($plugin->version)) {
+        // Prior to 2.2, component property wasn't required.
+        return array('component' => 'unknown', 'version' => $plugin->version);
+    } else if (isset($module->version)) {
+        return array('component' => 'unknown', 'version' => $module->version);
     } else {
         return false;
     }
@@ -214,6 +245,10 @@ function test_date_format($component, $thisversion) {
     if (strlen($thisversion) != 10) {
         echo "ERROR: $component version number $thisversion is not in yyyymmddxx format\n";
     }
+    if (!is_numeric($thisversion)) {
+        echo "ERROR: $component version number $thisversion is not numeric\n";
+    }
+
     $year = substr($thisversion, 0, 4);
     $thisyear = intval(date('Y'));
     //year could theoretically be anywhere from 1999 to the present
@@ -230,7 +265,7 @@ function test_date_format($component, $thisversion) {
     }
     //after sanity checking the individual numbers, finally run through checkdate()
     if (!checkdate($month, $day, $year)) {
-        echo "ERROR: $component version $thisversion - {$year}{$month}{$day} is not a valid date\n";
+        echo "ERROR: $component version $thisversion - {$year}/{$month}/{$day} is not a valid date\n";
     }
     $bump = substr($thisversion, 8, 2);
     if ($bump < 0 || $bump > 99) {
