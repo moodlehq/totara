031d4b81fa76f71dc04996a1f025667e6dcf84aa
David Curry <david.curry@totaralms.com>
2013-11-11 11:36:04 +1300

    T-11609 general: Fix upgrade issues caused by Moodle 2.5.3 merge
    Change-Id: If727ea6fa43a3a5d979a093c0a9cb8ed489c8f41
    Reviewed-on: https://review.totaralms.com/4094
    Reviewed-by: Simon Coggins <simon.coggins@totaralms.com>
    Reviewed-by: Yuliya Bozhko <yuliya.bozhko@totaralms.com>
    Tested-by: Jenkins Automation <jenkins@totaralms.com>

--- admin/tool/generator/version.php
+++ admin/tool/generator/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2013051402.04;
+$plugin->version = 2013090200;
 $plugin->requires = 2013050100;
 $plugin->component = 'tool_generator';
