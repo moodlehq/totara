253d213a2fdeb7f4431c0f3d6b1d82217a56c738
Oleg Demeshev <oleg.demeshev@totaralms.com>
2013-11-14 12:13:13 +1300

    T-11546 program: Fix the update exception if the user is already assigned to program
    Change-Id: Ia3a75a702702260f7fdffdc24535b8ab1231c154
    Reviewed-on: https://review.totaralms.com/4124
    Tested-by: Jenkins Automation <jenkins@totaralms.com>
    Reviewed-by: Moises Burgos <moises.burgos@totaralms.com>
    Reviewed-by: David Curry <david.curry@totaralms.com>

--- totara/program/program.class.php
+++ totara/program/program.class.php
@@ -1383,25 +1383,27 @@ class program {
      *
      */
     public function update_exceptions($userid, $assignment, $timedue) {
-        // Changes are being made so old exceptions are no longer
-        // relevant
+        // Changes are being made so old exceptions are no longer relevant.
         prog_exceptions_manager::delete_exceptions_by_assignment($assignment->id, $userid);
-        if ($timedue == COMPLETION_TIME_NOT_SET) {
-            return false;
-        }
         $now = time();
-        $total_time_allowed = $this->content->get_total_time_allowance();
-        $time_until_duedate = $timedue - $now;
+
+        if ($this->assigned_to_users_non_required_learning($userid)) {
+            $this->exceptionsmanager->raise_exception(EXCEPTIONTYPE_ALREADY_ASSIGNED, $userid, $assignment->id, $now);
+            return true;
+        }
 
         if ($timedue == COMPLETION_TIME_UNKNOWN) {
             $this->exceptionsmanager->raise_exception(EXCEPTIONTYPE_COMPLETION_TIME_UNKNOWN, $userid, $assignment->id, $now);
             return true;
-        } else if ($time_until_duedate < $total_time_allowed) {
-            $this->exceptionsmanager->raise_exception(EXCEPTIONTYPE_TIME_ALLOWANCE, $userid, $assignment->id, $now);
-            return true;
-        } else if ($this->assigned_to_users_non_required_learning($userid)) {
-            $this->exceptionsmanager->raise_exception(EXCEPTIONTYPE_ALREADY_ASSIGNED, $userid, $assignment->id, $now);
-            return true;
+        }
+
+        if ($timedue != COMPLETION_TIME_NOT_SET) {
+            $total_time_allowed = $this->content->get_total_time_allowance();
+            $time_until_duedate = $timedue - $now;
+            if ($time_until_duedate < $total_time_allowed) {
+                $this->exceptionsmanager->raise_exception(EXCEPTIONTYPE_TIME_ALLOWANCE, $userid, $assignment->id, $now);
+                return true;
+            }
         }
 
         return false;
